20b7803869a16a4208953eb8b65d6bda1eff2ffe
Th1nkK1D <witheep@gmail.com>
2023-09-05 14:37:55 +0700

    feat: customize typography system

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,22 @@
 <script>
-	import { DataTableSkeleton } from 'carbon-components-svelte';
+	import { Button } from 'carbon-components-svelte';
 </script>
 
 <div class="p-4 space-y-6">
-	<h1>Parliament Watch 👀</h1>
+	<h1 class="type-fluid-display-01">Parliament Watch 👀</h1>
 	<p>Coming soon...</p>
-	<DataTableSkeleton />
+	<Button>Button</Button>
+
+	{#each ['body-compact', 'body', 'label', 'helper-text', 'heading-compact', 'heading'] as typo}
+		{#each [1, 2] as variance}
+			{@const token = `${typo}-0${variance}`}
+			<p class={token}>{token}</p>
+		{/each}
+	{/each}
+	<p class="heading-03">heading-03</p>
+	<p class="fluid-heading-03">fluid-heading-03</p>
+	<p class="fluid-heading-04">fluid-heading-04</p>
+	<p class="fluid-heading-05">fluid-heading-05</p>
+	<p class="fluid-display-01">fluid-display-01</p>
+	<p class="fluid-display-03">fluid-display-03</p>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
+	corePlugins: {
+		preflight: false
+	},
 	theme: {
+		fontFamily: {
+			serif: ['Kondolar Thai', ...defaultTheme.fontFamily.serif],
+			sans: ['IBM Plex Sans Thai Looped', ...defaultTheme.fontFamily.sans]
+		},
 		extend: {}
 	},
 	plugins: []
