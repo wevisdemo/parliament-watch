6fe2e32c9174666d28e89faf0f8c7e38d36db123
Th1nkK1D <witheep@gmail.com>
2023-09-06 15:36:47 +0700

    feat: custom carbon white theme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,31 @@
 <script>
-	import { Button } from 'carbon-components-svelte';
+	import { Button, DataTableSkeleton } from 'carbon-components-svelte';
 </script>
 
-<div class="p-4 space-y-6">
+<div class="p-4 space-y-6 min-h-screen h-full">
 	<h1 class="fluid-display-01 text-blue-60">Parliament Watch 👀</h1>
 	<p>Coming soon...</p>
-	<Button>Button</Button>
 
-	{#each ['body-compact', 'body', 'label', 'helper-text', 'heading-compact', 'heading'] as typo}
-		{#each [1, 2] as variance}
-			{@const token = `${typo}-0${variance}`}
-			<p class={token}>{token}</p>
+	<div class="flex flex-row">
+		<Button>primary</Button>
+		<Button kind="secondary">secondary</Button>
+		<Button kind="danger">danger</Button>
+	</div>
+
+	<DataTableSkeleton showHeader={false} showToolbar={false} size="tall" />
+
+	<div>
+		{#each ['body-compact', 'body', 'label', 'helper-text', 'heading-compact', 'heading'] as typo}
+			{#each [1, 2] as variance}
+				{@const token = `${typo}-0${variance}`}
+				<p class={token}>{token}</p>
+			{/each}
 		{/each}
-	{/each}
-	<p class="heading-03">heading-03</p>
-	<p class="fluid-heading-03">fluid-heading-03</p>
-	<p class="fluid-heading-04">fluid-heading-04</p>
-	<p class="fluid-heading-05">fluid-heading-05</p>
-	<p class="fluid-display-01">fluid-display-01</p>
-	<p class="fluid-display-03">fluid-display-03</p>
+		<p class="heading-03">heading-03</p>
+		<p class="fluid-heading-03">fluid-heading-03</p>
+		<p class="fluid-heading-04">fluid-heading-04</p>
+		<p class="fluid-heading-05">fluid-heading-05</p>
+		<p class="fluid-display-01">fluid-display-01</p>
+		<p class="fluid-display-03">fluid-display-03</p>
+	</div>
 </div>
