0bba9b61a962b2b1a6aca58071d5c0b1313038ab
rootEnginear <rootEnginear@users.noreply.github.com>
2023-09-19 01:04:23 +0700

    feat(wev-12): add layout breadcrumb

---
+++ src/routes/politicians/[id]/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { Breadcrumb, BreadcrumbItem } from 'carbon-components-svelte';
+</script>
+
+<Breadcrumb noTrailingSlash class="px-4 py-2 font-sans body-compact-01">
+	<BreadcrumbItem href="/">หน้าหลัก</BreadcrumbItem>
+	<BreadcrumbItem href="/politicians">สส. ชุดที่ 26</BreadcrumbItem>
+	<BreadcrumbItem href="/politicians/ปิยบุตร-แสงกนกกุล" isCurrentPage>ปิยบุตร</BreadcrumbItem>
+</Breadcrumb>
+<slot />
