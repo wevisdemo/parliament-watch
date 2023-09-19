13ab22aa1be4d6ea9768d27f4cdaae27c9ddbcae
rootEnginear <rootEnginear@users.noreply.github.com>
2023-09-20 01:06:41 +0700

    fix(wev-12): breadcrumb use page data, fix mobile

--- src/routes/politicians/[id]/+layout.svelte
+++ src/routes/politicians/[id]/+layout.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
 	import { Breadcrumb, BreadcrumbItem } from 'carbon-components-svelte';
+
+	import { page } from '$app/stores';
+
+	const { politician } = $page.data;
 </script>
 
-<Breadcrumb noTrailingSlash class="px-4 py-2 font-sans body-compact-01">
+<Breadcrumb
+	noTrailingSlash
+	class="px-4 py-2 font-sans body-compact-01 [&>.bx--breadcrumb]:flex [&>.bx--breadcrumb]:flex-wrap"
+>
 	<BreadcrumbItem href="/">หน้าหลัก</BreadcrumbItem>
 	<BreadcrumbItem href="/politicians">สส. ชุดที่ 26</BreadcrumbItem>
-	<BreadcrumbItem href="/politicians/ปิยบุตร-แสงกนกกุล" isCurrentPage>ปิยบุตร</BreadcrumbItem>
+	<BreadcrumbItem href="/politicians/{politician.firstname}-{politician.lastname}" isCurrentPage
+		>{politician.firstname}</BreadcrumbItem
+	>
 </Breadcrumb>
 <slot />
