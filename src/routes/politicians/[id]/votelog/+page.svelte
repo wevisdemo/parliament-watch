047ea15404c17f3b106d3b5c692876346c409a43
rootEnginear <rootenginear@users.noreply.github.com>
2023-09-23 16:33:45 +0700

    feat: add votelog filter

--- src/routes/politicians/[id]/votelog/+page.svelte
+++ src/routes/politicians/[id]/votelog/+page.svelte
@@ -4,6 +4,7 @@
 		BreadcrumbItem,
 		Button,
 		ButtonSet,
+		FormGroup,
 		InlineNotification,
 		Pagination,
 		Search
@@ -11,8 +12,9 @@
 	import Download from 'carbon-icons-svelte/lib/Download.svelte';
 	import Minimize from 'carbon-icons-svelte/lib/Minimize.svelte';
 	import TableSplit from 'carbon-icons-svelte/lib/TableSplit.svelte';
-
 	import { DataTable } from 'carbon-components-svelte';
+	import { Checkbox } from 'carbon-components-svelte';
+	import Filter from 'carbon-icons-svelte/lib/Filter.svelte';
 
 	export let data;
 
@@ -27,6 +29,10 @@
 	}));
 	let pageSize = 10;
 	let page = 1;
+
+	let showFilter = true;
+
+	let showAllCatg = false;
 </script>
 
 <Breadcrumb
@@ -69,23 +75,88 @@
 </header>
 
 <div class="flex gap-1 bg-ui-01 min-h-screen">
-	<div class="sticky top-0 max-h-screen flex flex-col bg-white w-[250px] flex-[0_0_250px]">
-		<div class="sticky top-0 flex w-full pl-6">
-			<Search class="flex-1 !px-12" placeholder="ชื่อมติ หรือ คำที่เกี่ยวข้อง" light />
+	{#if showFilter}
+		<div
+			class="fixed w-full overscroll-none md:sticky top-0 max-h-screen flex flex-col bg-white md:w-[250px] flex-[0_0_250px] z-10"
+		>
+			<div class="sticky top-0 flex w-full pl-6">
+				<Search class="flex-1 !px-12" placeholder="ชื่อมติ หรือ คำที่เกี่ยวข้อง" light />
+				<Button
+					kind="ghost"
+					icon={Minimize}
+					iconDescription="ซ่อนตัวเลือก"
+					tooltipPosition="right"
+					tooltipAlignment="end"
+					on:click={() => {
+						showFilter = false;
+					}}
+				/>
+			</div>
+			<div class="flex-1 overflow-y-auto py-4 px-6">
+				<FormGroup legendText="สมัยการทำงาน">
+					<Checkbox id="era-26" labelText="สภาผู้แทนราษฎรชุดที่ 26 (2566 - ปัจจุบัน)" />
+					<Checkbox id="era-25" labelText="สภาผู้แทนราษฎรชุดที่ 25 (2563 - 2566)" />
+				</FormGroup>
+				<FormGroup legendText="ประเภทการลงมติ">
+					<Checkbox id="type-yes" labelText="เห็นด้วย (xxx)" />
+					<Checkbox id="type-no" labelText="ไม่เห็นด้วย (xxx)" />
+					<Checkbox id="type-abstain" labelText="งดออกเสียง (xxx)" />
+					<Checkbox id="type-novote" labelText="ไม่ลงคะแนน (xxx)" />
+					<Checkbox id="type-absent" labelText="ลา/ขาดประชุม (xxx)" />
+					<Checkbox id="type-other" labelText="อื่นๆ (xxx)" />
+				</FormGroup>
+				<FormGroup legendText="เงื่อนไขพิเศษ">
+					<Checkbox id="special-different" labelText="ลงมติต่างจากเสียงส่วนใหญ่ในพรรค (xxx)" />
+					<Checkbox id="special-follow" labelText="ลงมติเหมือนเสียงส่วนใหญ่ในพรรค (xxx)" />
+				</FormGroup>
+				<FormGroup legendText="หมวดมติ (1&nbsp;มติ มีได้มากกว่า 1&nbsp;หมวด)" class="mb-0">
+					<Checkbox id="catg-เศรษฐกิจ" labelText="เศรษฐกิจ (xxx)" />
+					<Checkbox id="catg-ขนส่งสาธารณะ" labelText="ขนส่งสาธารณะ (xxx)" />
+					<Checkbox id="catg-แก้รัฐธรรมนูญ" labelText="แก้รัฐธรรมนูญ (xxx)" />
+					<Checkbox id="catg-ที่อยู่อาศัย" labelText="ที่อยู่อาศัย (xxx)" />
+					{#if showAllCatg}
+						<Checkbox id="catg-สวัสดิการ" labelText="สวัสดิการ (xxx)" />
+						<Checkbox id="catg-การศึกษา" labelText="การศึกษา (xxx)" />
+						<Checkbox id="catg-สิ่งแวดล้อม" labelText="สิ่งแวดล้อม (xxx)" />
+						<Checkbox id="catg-สังคม" labelText="สังคม (xxx)" />
+						<Button
+							class="underline"
+							kind="ghost"
+							size="small"
+							on:click={() => {
+								showAllCatg = false;
+							}}>แสดงน้อยลง</Button
+						>
+					{:else}
+						<Button
+							class="underline"
+							kind="ghost"
+							size="small"
+							on:click={() => {
+								showAllCatg = true;
+							}}>ดูทั้งหมด (10)</Button
+						>
+					{/if}
+				</FormGroup>
+			</div>
+			<div class="flex gap-[1px] sticky bottom-0 body-compact-01 bg-white">
+				<Button class="flex-1 min-w-0 pr-4" kind="tertiary">ล้างตัวเลือก</Button>
+				<Button class="flex-1 min-w-0 pr-4">ดูที่เลือก</Button>
+			</div>
+		</div>
+	{:else}
+		<div class="fixed left-4 bottom-14 z-20">
 			<Button
-				kind="ghost"
-				icon={Minimize}
-				iconDescription="ซ่อนตัวเลือก"
-				tooltipPosition="right"
-				tooltipAlignment="end"
+				tooltipAlignment="start"
+				tooltipPosition="top"
+				iconDescription="แสดงตัวเลือก"
+				icon={Filter}
+				on:click={() => {
+					showFilter = true;
+				}}
 			/>
 		</div>
-		<div class="flex-1 overflow-y-auto">FILTERS</div>
-		<div class="flex gap-[1px] sticky bottom-0 body-compact-01">
-			<Button class="flex-1 min-w-0 pr-4" kind="tertiary">ล้างตัวเลือก</Button>
-			<Button class="flex-1 min-w-0 pr-4">ดูที่เลือก</Button>
-		</div>
-	</div>
+	{/if}
 	<div class="flex-1 bg-white">
 		<DataTable
 			class="[&>*>.bx--data-table--sticky-header]:max-h-[calc(100vh-40px)]"
