33bb8bc0ee46c3545eb3810ec2d47dab0e7b7802
Th1nkK1D <witheep@gmail.com>
2023-09-26 15:27:52 +0700

    feat(wev-12): provide filterOptions data in votelog

--- src/routes/politicians/[id]/votelog/+page.ts
+++ src/routes/politicians/[id]/votelog/+page.ts
@@ -1,5 +1,7 @@
 import type { Politician } from '$models/politician.js';
 import { DefaultVotingResult, type VoteOption, type Voting } from '$models/voting.js';
+import type { Assembly } from '$models/assembly';
+import { gov35, rep26 } from '../../../../mocks/data/assembly.js';
 import { defaultVoteOptions, mockCategory, passedVoting } from '../../../../mocks/data/voting.js';
 
 interface VotingSummary
@@ -9,6 +11,11 @@ interface VotingSummary
 	> {
 	voteOption: VoteOption;
 	isVoteAlignWithPartyMajority: boolean;
+}
+
+interface FilterOptions {
+	assemblies: Assembly[];
+	categories: string[];
 }
 
 type PoliticianSummary = Pick<Politician, 'id' | 'prefix' | 'firstname' | 'lastname'>;
@@ -23,19 +30,22 @@ export async function load({ params }) {
 		lastname
 	};
 
-	const votings: VotingSummary[] = new Array(100)
-		.fill(passedVoting)
-		.map(({ title, date, participatedAssembleIds }, i) => ({
-			id: i,
-			title,
-			date,
-			participatedAssembleIds,
-			category: mockCategory[i % mockCategory.length],
-			result: i % 3 ? DefaultVotingResult.Passed : DefaultVotingResult.Failed,
-			files: i % 2 ? [{ label: 'some file', url: '/' }] : [],
-			voteOption: defaultVoteOptions[i & defaultVoteOptions.length],
-			isVoteAlignWithPartyMajority: i % 5 !== 0
-		}));
+	const filterOptions: FilterOptions = {
+		assemblies: [rep26, gov35],
+		categories: mockCategory
+	};
 
-	return { politician, votings };
+	const votings: VotingSummary[] = new Array(100).fill(passedVoting).map(({ title, date }, i) => ({
+		id: i,
+		title,
+		date,
+		participatedAssembleIds: [i % 2 ? rep26.id : gov35.id],
+		category: mockCategory[i % mockCategory.length],
+		result: i % 3 ? DefaultVotingResult.Passed : DefaultVotingResult.Failed,
+		files: i % 2 ? [{ label: 'some file', url: '/' }] : [],
+		voteOption: defaultVoteOptions[i & defaultVoteOptions.length],
+		isVoteAlignWithPartyMajority: i % 5 !== 0
+	}));
+
+	return { politician, filterOptions, votings };
 }
