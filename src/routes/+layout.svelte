159bbb8d8efef1fb7d084adf55ba84cb44d38deb
rootEnginear <rootEnginear@users.noreply.github.com>
2023-09-27 17:34:19 +0700

    fix: tailwind fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
-	import '../styles/index.css';
 	import '../styles/carbon/pre-compiled.css';
 	import '../styles/carbon/colors.scss';
+	import '../styles/index.css';
 </script>
 
 <slot />
