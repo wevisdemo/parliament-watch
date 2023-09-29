3235a0d69a1ed98512689056513cb190c5ad3d03
rootEnginear <rootEnginear@users.noreply.github.com>
2023-09-29 16:05:19 +0700

    feat(wev-50): extract datapage

---
+++ src/components/DataPage.svelte
@@ -0,0 +1,260 @@
+<script lang="ts" context="module">
+	export interface FilterChoice {
+		label: string;
+		extension?: string | number;
+		value: string | number | boolean;
+	}
+
+	export interface FilterGroup {
+		key: string;
+		legend: string;
+		choices: FilterChoice[];
+	}
+
+	export type SelectedFilterType = Record<FilterGroup['key'], FilterChoice['value'][]>;
+</script>
+
+<script lang="ts">
+	import {
+		Breadcrumb,
+		BreadcrumbItem,
+		Button,
+		Checkbox,
+		DataTable,
+		DataTableSkeleton,
+		FormGroup,
+		Pagination,
+		PaginationSkeleton,
+		Search
+	} from 'carbon-components-svelte';
+	import Download from 'carbon-icons-svelte/lib/Download.svelte';
+	import Filter from 'carbon-icons-svelte/lib/Filter.svelte';
+	import FilterEdit from 'carbon-icons-svelte/lib/FilterEdit.svelte';
+	import Minimize from 'carbon-icons-svelte/lib/Minimize.svelte';
+	import TableSplit from 'carbon-icons-svelte/lib/TableSplit.svelte';
+	import { onMount } from 'svelte';
+
+	// Just props
+	export let breadcrumbList: {
+		label: string;
+		url: string;
+	}[];
+	export let filterList: FilterGroup[];
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	export let filteredData: { id: any; [key: string]: any }[];
+	export let tableHeader: { key: string; value: string }[];
+
+	// For binding
+	export let searchQuery = '';
+	export let selectedFilter: SelectedFilterType = Object.fromEntries(
+		filterList.map((group) => [group.key, group.choices.map((choice) => choice.value)])
+	);
+	export let isFilterSomeFalse = true;
+	export let isFilterAllFalse = true;
+
+	// Reactive
+	let tablePageSize = 10;
+	let tableCurrentPage = 1;
+
+	const ALL_FILTER_COUNT = Object.values(filterList)
+		.map((e) => e.choices.length)
+		.reduce((a, c) => a + c);
+
+	const filterTickAll = (value = true) => {
+		if (value) {
+			selectedFilter = Object.fromEntries(
+				filterList.map((group) => [group.key, group.choices.map((choice) => choice.value)])
+			);
+		} else {
+			selectedFilter = Object.fromEntries(filterList.map((group) => [group.key, []]));
+		}
+	};
+
+	$: tickedFilterCount = Object.values(selectedFilter)
+		.map((e) => e.length)
+		.reduce((a, c) => a + c);
+	$: isFilterSomeFalse = tickedFilterCount < ALL_FILTER_COUNT;
+	$: isFilterAllFalse = tickedFilterCount === 0;
+
+	let showFilter = true;
+	export let mounted = false;
+	onMount(() => {
+		mounted = true;
+		showFilter = window.matchMedia(`(min-width: 672px)`).matches;
+	});
+</script>
+
+<div class="flex flex-col min-h-screen">
+	<Breadcrumb
+		noTrailingSlash
+		class="px-4 py-2 body-compact-01 [&>.bx--breadcrumb]:flex [&>.bx--breadcrumb]:flex-wrap"
+	>
+		{#each breadcrumbList as breadcrumbItem, idx (breadcrumbItem.url)}
+			<BreadcrumbItem href={breadcrumbItem.url} isCurrentPage={idx === breadcrumbList.length - 1}
+				>{breadcrumbItem.label}</BreadcrumbItem
+			>
+		{/each}
+	</Breadcrumb>
+	<header class="px-4 py-3 bg-ui-01 md:px-16">
+		<div class="flex flex-col gap-1 md:flex-row md:gap-16 md:items-end">
+			<div class="flex-1">
+				<slot />
+			</div>
+			<div class="flex flex-col gap-2 border border-solid border-ui-03 rounded-sm p-3">
+				<div class="flex items-center gap-1">
+					<Download />
+					<h2 class="heading-01">ดาวน์โหลดข้อมูล</h2>
+				</div>
+				<!-- TODO: add link -->
+				<a href="/" class="flex items-center gap-1 mr-auto helper-text-01">
+					<TableSplit />
+					<span>ผลการลงมติรายคน</span>
+				</a>
+			</div>
+		</div>
+		<Search
+			class="md:hidden {!mounted ? '-mt-4' : ''}"
+			searchClass="md:hidden mt-2"
+			placeholder="ชื่อมติ หรือ คำที่เกี่ยวข้อง"
+			light
+			bind:value={searchQuery}
+			skeleton={!mounted}
+		/>
+	</header>
+	<div class="flex-1 flex gap-1 bg-ui-01 w-full">
+		{#if showFilter}
+			<div
+				class="fixed w-full h-screen md:h-auto md:max-h-screen overscroll-none md:sticky top-0 flex flex-col bg-white md:w-[250px] flex-[0_0_250px] z-10"
+				class:md:flex={!mounted}
+				class:hidden={!mounted}
+			>
+				<div class="sticky top-0 flex w-full pl-6">
+					<Search
+						class="flex-1 {!mounted ? '-mt-6' : ''}"
+						placeholder="ชื่อมติ หรือ คำที่เกี่ยวข้อง"
+						light
+						bind:value={searchQuery}
+						skeleton={!mounted}
+					/>
+					<Button
+						kind="ghost"
+						icon={Minimize}
+						iconDescription="ซ่อนตัวเลือก"
+						tooltipPosition="right"
+						tooltipAlignment="end"
+						on:click={() => {
+							showFilter = false;
+						}}
+						skeleton={!mounted}
+					/>
+				</div>
+				<div class="flex-[1_1_auto] h-0 overflow-y-scroll py-4 px-6">
+					{#each filterList as optionGroup, idx (optionGroup.key)}
+						<FormGroup
+							legendText={optionGroup.legend}
+							class={idx === filterList.length - 1 ? 'mb-0' : ''}
+						>
+							{#each optionGroup.choices as choice (choice.label)}
+								<Checkbox
+									bind:group={selectedFilter[optionGroup.key]}
+									value={choice.value}
+									labelText={choice.label +
+										(choice.extension !== undefined ? ` (${choice.extension})` : '')}
+									skeleton={!mounted}
+								/>
+							{/each}
+						</FormGroup>
+					{/each}
+				</div>
+				<div class="flex gap-[1px] sticky bottom-0 body-compact-01 bg-white">
+					{#if isFilterAllFalse}
+						<Button
+							class="flex-1 min-w-0 pr-4"
+							kind="secondary"
+							on:click={() => filterTickAll()}
+							skeleton={!mounted}>เลือกทั้งหมด</Button
+						>
+					{:else}
+						<Button
+							class="flex-1 min-w-0 pr-4"
+							kind="tertiary"
+							on:click={() => filterTickAll(false)}
+							skeleton={!mounted}>ล้างตัวเลือก</Button
+						>
+					{/if}
+					<Button
+						class="flex-1 min-w-0 pr-4 md:hidden"
+						on:click={() => {
+							showFilter = false;
+						}}
+						skeleton={!mounted}>ดูที่เลือก</Button
+					>
+				</div>
+			</div>
+		{:else}
+			<div class="fixed left-4 bottom-14 z-20">
+				<Button
+					tooltipAlignment="start"
+					tooltipPosition="top"
+					iconDescription="แสดงตัวเลือก"
+					icon={isFilterSomeFalse ? FilterEdit : Filter}
+					kind={isFilterSomeFalse ? 'secondary' : 'primary'}
+					on:click={() => {
+						showFilter = true;
+					}}
+					skeleton={!mounted}
+				/>
+			</div>
+		{/if}
+		<div class="flex-1 flex flex-col bg-white">
+			{#if mounted}
+				<div class="overflow-x-auto overflow-y-hidden">
+					<DataTable
+						class="pt-0 w-0 min-w-full"
+						size="tall"
+						headers={tableHeader}
+						rows={filteredData}
+						pageSize={tablePageSize}
+						page={tableCurrentPage}
+					>
+						<svelte:fragment slot="cell" let:cell>
+							<slot name="table" cellKey={cell.key} cellValue={cell.value} />
+						</svelte:fragment>
+					</DataTable>
+				</div>
+				{#if filteredData.length === 0}
+					<div
+						class="h-10 body-compact-01 text-gray-60 px-4 flex items-center border-solid border-b border-b-ui-03"
+					>
+						ไม่พบข้อมูลที่ค้นหา
+					</div>
+				{/if}
+				<div class="flex-1" />
+				<Pagination
+					class="sticky bottom-0 overflow-x-hidden"
+					bind:pageSize={tablePageSize}
+					bind:page={tableCurrentPage}
+					totalItems={filteredData.length}
+					pageSizeInputDisabled
+					forwardText="หน้าถัดไป"
+					backwardText="หน้าก่อนหน้า"
+					itemRangeText={(min, max, total) => `${min} – ${max} จาก ${total} มติ`}
+					pageRangeText={(_, total) => `จาก ${total} หน้า`}
+				/>
+			{:else}
+				<div class="overflow-x-auto overflow-y-hidden">
+					<DataTableSkeleton
+						class="pt-0 w-0 min-w-full"
+						size="tall"
+						headers={tableHeader}
+						rows={10}
+						showHeader={false}
+						showToolbar={false}
+					/>
+				</div>
+				<div class="flex-1" />
+				<PaginationSkeleton />
+			{/if}
+		</div>
+	</div>
+</div>
--- src/routes/politicians/[id]/votelog/+page.svelte
+++ src/routes/politicians/[id]/votelog/+page.svelte
@@ -1,324 +1,188 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import VotingResultTag from '$components/politicians/VotingResultTag.svelte';
+	import DataPage from '$components/DataPage.svelte';
 	import VotingOptionTag from '$components/politicians/VotingOptionTag.svelte';
-	import {
-		Breadcrumb,
-		BreadcrumbItem,
-		Button,
-		Checkbox,
-		DataTable,
-		FormGroup,
-		InlineNotification,
-		Pagination,
-		Search
-	} from 'carbon-components-svelte';
-	import DocumentPdf from 'carbon-icons-svelte/lib/DocumentPdf.svelte';
-	import Download from 'carbon-icons-svelte/lib/Download.svelte';
-	import Filter from 'carbon-icons-svelte/lib/Filter.svelte';
-	import FilterEdit from 'carbon-icons-svelte/lib/FilterEdit.svelte';
-	import Minimize from 'carbon-icons-svelte/lib/Minimize.svelte';
-	import TableSplit from 'carbon-icons-svelte/lib/TableSplit.svelte';
-	import { onMount } from 'svelte';
+	import VotingResultTag from '$components/politicians/VotingResultTag.svelte';
 	import { DefaultVoteOption, type CustomVoteOption } from '$models/voting.js';
+	import { InlineNotification } from 'carbon-components-svelte';
+	import DocumentPdf from 'carbon-icons-svelte/lib/DocumentPdf.svelte';
+	import { onMount } from 'svelte';
+	import type { FilterGroup, SelectedFilterType } from '../../../../components/DataPage.svelte';
 
 	export let data;
 	const { politician, filterOptions, votings } = data;
 
-	const VOTEOPTIONS = [
-		DefaultVoteOption.Agreed,
-		DefaultVoteOption.Disagreed,
-		DefaultVoteOption.Novote,
-		DefaultVoteOption.Abstain,
-		DefaultVoteOption.Absent,
-		'อื่นๆ'
-	];
-	const VOTEDIRECTIONS = [
-		{ label: 'ลงมติต่างจากเสียงส่วนใหญ่ในพรรค', value: false },
-		{ label: 'ลงมติเหมือนเสียงส่วนใหญ่ในพรรค', value: true }
-	];
-
-	const FILTER_ASSEMBLY_DEFAULT = filterOptions.assemblies.map((e) => e.id);
-	const FILTER_VOTETYPE_DEFAULT = [...VOTEOPTIONS];
-	const FILTER_VOTEDIRECTION_DEFAULT = [...VOTEDIRECTIONS.map((e) => e.value)];
-	const FILTER_CATEGORY_DEFAULT = filterOptions.categories;
-	$: ALL_FILTER_COUNT =
-		FILTER_ASSEMBLY_DEFAULT.length +
-		FILTER_VOTETYPE_DEFAULT.length +
-		FILTER_VOTEDIRECTION_DEFAULT.length +
-		FILTER_CATEGORY_DEFAULT.length;
-
-	let tablePageSize = 10;
-	let tableCurrentPage = 1;
-
-	let searchQuery = '';
-	let showFilter = true;
-	let filterAssembly: string[] = [...FILTER_ASSEMBLY_DEFAULT];
-	let filterVoteType: string[] = [...FILTER_VOTETYPE_DEFAULT];
-	let filterVoteDirection: boolean[] = [...FILTER_VOTEDIRECTION_DEFAULT];
-	let filterCatg: string[] = [...FILTER_CATEGORY_DEFAULT];
-	$: tickedFilterCount =
-		filterAssembly.length + filterVoteType.length + filterVoteDirection.length + filterCatg.length;
-
-	const filterTickAll = (value = true) => {
-		if (value) {
-			filterAssembly = [...FILTER_ASSEMBLY_DEFAULT];
-			filterVoteType = [...FILTER_VOTETYPE_DEFAULT];
-			filterVoteDirection = [...FILTER_VOTEDIRECTION_DEFAULT];
-			filterCatg = [...FILTER_CATEGORY_DEFAULT];
-		} else {
-			filterAssembly = [];
-			filterVoteType = [];
-			filterVoteDirection = [];
-			filterCatg = [];
-		}
-	};
-
 	const generalVoteType = (voteOption: DefaultVoteOption | CustomVoteOption) =>
 		typeof voteOption === 'string' ? (voteOption as string) : 'อื่นๆ';
 
-	$: isFilterSomeFalse = tickedFilterCount < ALL_FILTER_COUNT;
-	$: isFilterAllFalse = tickedFilterCount === 0;
+	const formatThaiYear = (date: Date | undefined) => {
+		if (!date) return;
+		return date.toLocaleString('th-TH', { year: 'numeric' });
+	};
 
-	$: filteredData = isFilterAllFalse
-		? []
-		: votings.filter((votingDataEntry) => {
-				const search = searchQuery.trim();
-				if (search && !votingDataEntry.title.includes(search)) return;
-				return (
-					filterAssembly.some((assemblyId) =>
-						votingDataEntry.participatedAssembleIds.includes(assemblyId)
-					) &&
-					filterVoteType.includes(generalVoteType(votingDataEntry.voteOption)) &&
-					filterVoteDirection.includes(votingDataEntry.isVoteAlignWithPartyMajority) &&
-					filterCatg.includes(votingDataEntry.category)
-				);
-		  });
+	const votingFilterCount: Record<string, number> = {};
+	for (let vote of votings) {
+		const catg = vote.category;
+		votingFilterCount[catg] = (votingFilterCount[catg] ?? 0) + 1;
+		const type = generalVoteType(vote.voteOption);
+		votingFilterCount[type] = (votingFilterCount[type] ?? 0) + 1;
+		if (vote.isVoteAlignWithPartyMajority) {
+			votingFilterCount['align'] = (votingFilterCount['align'] ?? 0) + 1;
+		} else {
+			votingFilterCount['not-align'] = (votingFilterCount['not-align'] ?? 0) + 1;
+		}
+	}
 
-	let mounted = false;
+	const filterList: FilterGroup[] = [
+		{
+			key: 'filterAssembly',
+			legend: 'สมัยการทำงาน',
+			choices: filterOptions.assemblies.map((assembly) => ({
+				label: `${assembly.name}ชุดที่ ${assembly.term}`,
+				extension: `${formatThaiYear(assembly.startedAt)} - ${
+					formatThaiYear(assembly?.endedAt) ?? 'ปัจจุบัน'
+				}`,
+				value: assembly.id
+			}))
+		},
+		{
+			key: 'filterVoteType',
+			legend: 'ประเภทการลงมติ',
+			choices: [
+				DefaultVoteOption.Agreed,
+				DefaultVoteOption.Disagreed,
+				DefaultVoteOption.Novote,
+				DefaultVoteOption.Abstain,
+				DefaultVoteOption.Absent,
+				'อื่นๆ'
+			].map((type) => ({
+				label: type,
+				extension: votingFilterCount[type],
+				value: type
+			}))
+		},
+		{
+			key: 'filterVoteDirection',
+			legend: 'เงื่อนไขพิเศษ',
+			choices: [
+				{
+					label: 'ลงมติต่างจากเสียงส่วนใหญ่ในพรรค',
+					extension: votingFilterCount['not-align'],
+					value: false
+				},
+				{
+					label: 'ลงมติเหมือนเสียงส่วนใหญ่ในพรรค',
+					extension: votingFilterCount['align'],
+					value: true
+				}
+			]
+		},
+		{
+			key: 'filterCatg',
+			legend: 'หมวดมติ (1 มติ มีได้มากกว่า 1 หมวด)',
+			choices: filterOptions.categories.map((catg) => ({
+				label: catg,
+				extension: votingFilterCount[catg],
+				value: catg
+			}))
+		}
+	];
+
+	let searchQuery = '';
+	let isFilterAllFalse: boolean;
+	let selectedFilter: SelectedFilterType;
+
+	$: filteredData =
+		selectedFilter === undefined || isFilterAllFalse
+			? []
+			: votings.filter((votingDataEntry) => {
+					const search = searchQuery.trim();
+					if (search && !votingDataEntry.title.includes(search)) return;
+					const { filterAssembly, filterVoteType, filterVoteDirection, filterCatg } =
+						selectedFilter;
+					return (
+						filterAssembly.some((assemblyId) =>
+							votingDataEntry.participatedAssembleIds.includes(assemblyId as string)
+						) &&
+						filterVoteType.includes(generalVoteType(votingDataEntry.voteOption)) &&
+						filterVoteDirection.includes(votingDataEntry.isVoteAlignWithPartyMajority) &&
+						filterCatg.includes(votingDataEntry.category)
+					);
+			  });
+
 	onMount(() => {
-		mounted = true;
-		showFilter = window.matchMedia(`(min-width: 672px)`).matches;
 		switch ($page.url.searchParams.get('votetype')) {
 			case 'agreed':
-				filterVoteType = [DefaultVoteOption.Agreed];
+				selectedFilter.filterVoteType = [DefaultVoteOption.Agreed];
 				break;
 			case 'disagreed':
-				filterVoteType = [DefaultVoteOption.Disagreed];
+				selectedFilter.filterVoteType = [DefaultVoteOption.Disagreed];
 				break;
 			case 'absent':
-				filterVoteType = [DefaultVoteOption.Absent];
+				selectedFilter.filterVoteType = [DefaultVoteOption.Absent];
 				break;
 		}
 	});
 </script>
 
-<div class="flex flex-col min-h-screen">
-	<Breadcrumb
-		noTrailingSlash
-		class="px-4 py-2 body-compact-01 [&>.bx--breadcrumb]:flex [&>.bx--breadcrumb]:flex-wrap"
-	>
-		<BreadcrumbItem href="/">หน้าหลัก</BreadcrumbItem>
-		<BreadcrumbItem href="/politicians/{politician.id}">{politician.firstname}</BreadcrumbItem>
-		<BreadcrumbItem href="/politicians/{politician.id}/votelog" isCurrentPage
-			>ประวัติการลงมติ</BreadcrumbItem
+<DataPage
+	breadcrumbList={[
+		{ url: '/', label: 'หน้าหลัก' },
+		{ url: `/politicians/${politician.id}`, label: politician.firstname },
+		{ url: `/politicians/${politician.id}/votelog`, label: 'ประวัติการลงมติ' }
+	]}
+	{filterList}
+	{filteredData}
+	tableHeader={[
+		{ key: 'date', value: 'วันที่' },
+		{ key: 'title', value: 'ชื่อมติ' },
+		{ key: 'voteOption', value: 'การลงมติ' },
+		{ key: 'result', value: 'ผลลัพธ์' },
+		{ key: 'files', value: 'เอกสาร' }
+	]}
+	bind:searchQuery
+	bind:selectedFilter
+	bind:isFilterAllFalse
+>
+	<p class="heading-01">ประวัติการลงมติ</p>
+	<h1 class="fluid-heading-03 mb-1">
+		<a class="no-underline text-black hover:text-blue-70" href="/politicians/{politician.id}"
+			>{politician.prefix} {politician.firstname} {politician.lastname}</a
 		>
-	</Breadcrumb>
-	<header class="px-4 py-3 bg-ui-01 md:px-16">
-		<p class="heading-01">ประวัติการลงมติ</p>
-		<h1 class="fluid-heading-03 mb-1">
-			<a class="no-underline text-black hover:text-blue-70" href="/politicians/{politician.id}"
-				>{politician.prefix} {politician.firstname} {politician.lastname}</a
-			>
-		</h1>
-		<div class="flex flex-col gap-1 md:flex-row md:gap-16">
-			<div class="flex-1">
-				<InlineNotification
-					slot="header-extension"
-					class="h-full m-0 min-w-0 max-w-none"
-					lowContrast
-					kind="info"
-					subtitle="การประเมินพฤติกรรมการลงมติ จะพิจารณาเพียงชื่อมติไม่ได้ ควรศึกษาเนื้อหาของมตินั้นๆ ประกอบด้วย"
-				/>
-			</div>
-			<div class="flex flex-col gap-2 border border-solid border-ui-03 rounded-sm p-3">
-				<div class="flex items-center gap-1">
-					<Download />
-					<h2 class="heading-01">ดาวน์โหลดข้อมูล</h2>
-				</div>
-				<!-- TODO: add link -->
-				<a href="/" class="flex items-center gap-1 mr-auto helper-text-01">
-					<TableSplit />
-					<span>ผลการลงมติรายคน</span>
-				</a>
-			</div>
-		</div>
-		<Search
-			searchClass="md:hidden mt-2"
-			placeholder="ชื่อมติ หรือ คำที่เกี่ยวข้อง"
-			light
-			bind:value={searchQuery}
-		/>
-	</header>
-	<div class="flex-1 flex gap-1 bg-ui-01 w-full">
-		{#if showFilter}
-			<div
-				class="fixed w-full h-screen md:h-auto md:max-h-screen overscroll-none md:sticky top-0 flex flex-col bg-white md:w-[250px] flex-[0_0_250px] z-10"
-				class:md:flex={!mounted}
-				class:hidden={!mounted}
-			>
-				<div class="sticky top-0 flex w-full pl-6">
-					<Search
-						class="flex-1"
-						placeholder="ชื่อมติ หรือ คำที่เกี่ยวข้อง"
-						light
-						bind:value={searchQuery}
-					/>
-					<Button
-						kind="ghost"
-						icon={Minimize}
-						iconDescription="ซ่อนตัวเลือก"
-						tooltipPosition="right"
-						tooltipAlignment="end"
-						on:click={() => {
-							showFilter = false;
-						}}
-					/>
-				</div>
-				<div class="flex-[1_1_auto] h-0 overflow-y-scroll py-4 px-6">
-					<FormGroup legendText="สมัยการทำงาน">
-						{#each filterOptions.assemblies as assembly (assembly.id)}
-							<Checkbox
-								bind:group={filterAssembly}
-								value={assembly.id}
-								labelText="{assembly.name}ชุดที่ {assembly.term} ({assembly.startedAt.toLocaleString(
-									'th-TH',
-									{ year: 'numeric' }
-								)} - {assembly?.endedAt?.toLocaleString('th-TH', { year: 'numeric' }) ??
-									'ปัจจุบัน'})"
-							/>
-						{/each}
-					</FormGroup>
-					<FormGroup legendText="ประเภทการลงมติ">
-						{#each FILTER_VOTETYPE_DEFAULT as votetype (votetype)}
-							<Checkbox bind:group={filterVoteType} value={votetype} labelText="{votetype} (xxx)" />
-						{/each}
-					</FormGroup>
-					<FormGroup legendText="เงื่อนไขพิเศษ">
-						{#each VOTEDIRECTIONS as votedirection (votedirection.label)}
-							<Checkbox
-								bind:group={filterVoteDirection}
-								value={votedirection.value}
-								labelText="{votedirection.label} (xxx)"
-							/>
-						{/each}
-					</FormGroup>
-					<FormGroup legendText="หมวดมติ (1&nbsp;มติ มีได้มากกว่า 1&nbsp;หมวด)" class="mb-0">
-						{#each filterOptions.categories as catg (catg)}
-							<Checkbox bind:group={filterCatg} value={catg} labelText="{catg} (xxx)" />
-						{/each}
-					</FormGroup>
-				</div>
-				<div class="flex gap-[1px] sticky bottom-0 body-compact-01 bg-white">
-					{#if isFilterAllFalse}
-						<Button class="flex-1 min-w-0 pr-4" kind="secondary" on:click={() => filterTickAll()}
-							>เลือกทั้งหมด</Button
-						>
-					{:else}
-						<Button
-							class="flex-1 min-w-0 pr-4"
-							kind="tertiary"
-							on:click={() => filterTickAll(false)}>ล้างตัวเลือก</Button
-						>
-					{/if}
-					<Button
-						class="flex-1 min-w-0 pr-4 md:hidden"
-						on:click={() => {
-							showFilter = false;
-						}}>ดูที่เลือก</Button
+	</h1>
+	<InlineNotification
+		class="m-0 min-w-0 max-w-none"
+		lowContrast
+		kind="info"
+		subtitle="การประเมินพฤติกรรมการลงมติ จะพิจารณาเพียงชื่อมติไม่ได้ ควรศึกษาเนื้อหาของมตินั้นๆ ประกอบด้วย"
+	/>
+	<svelte:fragment slot="table" let:cellKey let:cellValue>
+		{#if cellKey === 'date'}
+			{cellValue.toLocaleDateString('th-TH', {
+				day: 'numeric',
+				month: 'short',
+				year: '2-digit'
+			})}
+		{:else if cellKey === 'title'}
+			<!-- TODO: Add link -->
+			<a class="text-text-01" href="/">{cellValue}</a>
+		{:else if cellKey === 'result'}
+			<VotingResultTag class="m-0 whitespace-nowrap" result={cellValue} />
+		{:else if cellKey === 'voteOption'}
+			<VotingOptionTag voteOption={cellValue} />
+		{:else if cellKey === 'files'}
+			{@const files = cellValue}
+			{#if files.length > 0}
+				{#each files as file (file)}
+					<a href={file.url} download title={file.label}
+						><DocumentPdf /><span class="sr-only">{file.label}</span></a
 					>
-				</div>
-			</div>
-		{:else}
-			<div class="fixed left-4 bottom-14 z-20">
-				<Button
-					tooltipAlignment="start"
-					tooltipPosition="top"
-					iconDescription="แสดงตัวเลือก"
-					icon={isFilterSomeFalse ? FilterEdit : Filter}
-					kind={isFilterSomeFalse ? 'secondary' : 'primary'}
-					on:click={() => {
-						showFilter = true;
-					}}
-				/>
-			</div>
-		{/if}
-		<div class="flex-1 flex flex-col bg-white">
-			<div class="overflow-x-auto overflow-y-hidden">
-				<DataTable
-					class="pt-0 w-0 min-w-full"
-					size="tall"
-					headers={[
-						{ key: 'date', value: 'วันที่' },
-						{ key: 'title', value: 'ชื่อมติ' },
-						{ key: 'voteOption', value: 'การลงมติ' },
-						{ key: 'result', value: 'ผลลัพธ์' },
-						{ key: 'files', value: 'เอกสาร' }
-					]}
-					rows={filteredData}
-					pageSize={tablePageSize}
-					page={tableCurrentPage}
-				>
-					<svelte:fragment slot="cell" let:cell>
-						{#if cell.key === 'date'}
-							{cell.value.toLocaleDateString('th-TH', {
-								day: 'numeric',
-								month: 'short',
-								year: '2-digit'
-							})}
-						{:else if cell.key === 'title'}
-							<!-- TODO: Add link -->
-							<a class="text-text-01" href="/">{cell.value}</a>
-						{:else if cell.key === 'result'}
-							<VotingResultTag class="m-0 whitespace-nowrap" result={cell.value} />
-						{:else if cell.key === 'voteOption'}
-							<VotingOptionTag voteOption={cell.value} />
-						{:else if cell.key === 'files'}
-							{@const files = cell.value}
-							{#if files.length > 0}
-								{#each files as file (file)}
-									<a href={file.url} download title={file.label}
-										><DocumentPdf /><span class="sr-only">{file.label}</span></a
-									>
-								{/each}
-							{:else}
-								-
-							{/if}
-						{:else}
-							{cell.value}
-						{/if}
-					</svelte:fragment>
-				</DataTable>
-			</div>
-			{#if filteredData.length === 0}
-				<div
-					class="h-10 body-compact-01 text-gray-60 px-4 flex items-center border-solid border-b border-b-ui-03"
-				>
-					ไม่พบข้อมูลที่ค้นหา
-				</div>
+				{/each}
+			{:else}
+				-
 			{/if}
-			<div class="flex-1" />
-			<Pagination
-				class="sticky bottom-0 overflow-x-hidden"
-				bind:pageSize={tablePageSize}
-				bind:page={tableCurrentPage}
-				totalItems={filteredData.length}
-				pageSizeInputDisabled
-				forwardText="หน้าถัดไป"
-				backwardText="หน้าก่อนหน้า"
-				itemRangeText={(min, max, total) => `${min} – ${max} จาก ${total} มติ`}
-				pageRangeText={(_, total) => `จาก ${total} หน้า`}
-			/>
-		</div>
-	</div>
-</div>
+		{:else}
+			{cellValue}
+		{/if}
+	</svelte:fragment>
+</DataPage>
