5b22e562b4ab13db79bf482091e751c560b69be1
rootEnginear <rootEnginear@users.noreply.github.com>
2023-10-03 18:01:03 +0700

    feat: add disclaimer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,16 @@
 	import '../styles/carbon/pre-compiled.css';
 	import '../styles/carbon/colors.scss';
 	import '../styles/index.css';
+
+	import { InlineNotification } from 'carbon-components-svelte';
 </script>
 
 <slot />
+<InlineNotification
+	class="fixed z-50 w-full left-0 bottom-0 m-0 max-w-none min-w-0"
+	lowContrast
+	kind="warning-alt"
+	title="คำเตือน:"
+	subtitle="เว็บไซต์นี้อยู่ระหว่างการพัฒนา ข้อมูลทั้งหมดบนเว็บไซต์ถูกสมมุติขึ้นเป็นการชั่วคราวเท่านั้น ไม่สามารถใช้อ้างอิงได้ในทุกกรณี"
+	hideCloseButton
+/>
