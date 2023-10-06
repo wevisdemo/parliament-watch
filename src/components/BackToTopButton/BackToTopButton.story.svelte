42d2c18d1b8d48b10090521931e7fa0a96de33a4
Natthaphong Ruengpanyawut <tee4cute@gmail.com>
2023-10-06 23:01:26 +0700

    feat(#7): just checked out and newly created component

---
+++ src/components/BackToTopButton/BackToTopButton.story.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { Hst } from '@histoire/plugin-svelte';
+	import BackToTopButton from './BackToTopButton.svelte';
+	export let Hst: Hst;
+</script>
+
+<Hst.Story title="BackToTopButton">
+	<BackToTopButton />
+</Hst.Story>
