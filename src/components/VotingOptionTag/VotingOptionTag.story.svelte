9b5f10c499ff71e462266d200003d61b1cd91175
Icyscools <woramat.ng@gmail.com>
2023-10-08 04:21:47 +0700

    feat(#13): add voting option tag component

---
+++ src/components/VotingOptionTag/VotingOptionTag.story.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type { Hst } from '@histoire/plugin-svelte';
+	import VotingOptionTag from './VotingOptionTag.svelte';
+	import { DefaultVoteOption } from '$models/voting';
+	export let Hst: Hst;
+</script>
+
+<Hst.Story title="VotingOptionTag">
+	<div class="flex flex-col gap-4">
+		<div>
+			<VotingOptionTag voteOption={DefaultVoteOption.Agreed} />
+			<VotingOptionTag voteOption={DefaultVoteOption.Disagreed} />
+			<VotingOptionTag voteOption={DefaultVoteOption.Novote} />
+			<VotingOptionTag voteOption={DefaultVoteOption.Abstain} />
+		</div>
+		<div>
+			<VotingOptionTag voteOption={{ label: 'ชื่อแคนดิเดต', colorIntensity: 1 }} />
+			<VotingOptionTag voteOption={{ label: 'ชื่อแคนดิเดต', colorIntensity: 0.75 }} />
+			<VotingOptionTag voteOption={{ label: 'ชื่อแคนดิเดต', colorIntensity: 0.5 }} />
+			<VotingOptionTag voteOption={{ label: 'ชื่อแคนดิเดต', colorIntensity: 0.25 }} />
+			<VotingOptionTag voteOption={{ label: 'ชื่อแคนดิเดต', colorIntensity: 0 }} />
+		</div>
+	</div>
+</Hst.Story>
