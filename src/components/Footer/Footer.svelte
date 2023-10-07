bacb3f74a67dadb8e3c1296914ef386da23aaace
Yuttakhan B <yuttakhanbaingen@gmail.com>
2023-10-07 19:59:06 +0700

    feat(wevisdemo#4): add content in footer information section

--- src/components/Footer/Footer.svelte
+++ src/components/Footer/Footer.svelte
@@ -1,9 +1,27 @@
-<div class="bg-white py-8 px-16">
-	<div>เว็บไซต์นี้สร้างโดย</div>
-	<div>WeVis</div>
-	<div>ซึ่งเป็น xxxx</div>
-	<div>ความถูกต้อง</div>
-	<div>Open source license</div>
+<div class="grid grid-cols-1 gap-4 bg-white py-8 px-16 text-sm">
+	<div>
+		เว็บไซต์นี้สร้างโดย<br />
+		WeVis<br />
+		ซึ่งเป็น… เริ่มสร้างเว็บไซต์นี้โดยมีจุดประสงค์เพื่อ… และได้รับการสนับสนุนจาก….
+	</div>
+	<div>
+		<div class="font-semibold">ความถูกต้องและการอ้างอิงข้อมูลในเว็บไซต์</div>
+		<div>
+			(พารากราฟสั้นๆ อธิบายประเด็นต่อไปนี้) ข้อมูลในเว็บไซต์นี้เรารวบรวมจาก…
+			โดยได้รวบรวมและอัปเดทข้อมูลเมื่อ(หรือทุกๆ)…ข้อมูลอาจมีความคลาดเคลื่อน
+			ซึ่งเป็นไปได้ว่าเกิดจาก…หากคุณต้องการนำข้อมูลไปใช้ในงานของคุณเราแนะนำให้คุณ….หรือหากมีข้อสงสัยว่าข้อมูลไม่ถูกต้อง
+			กรุณาติดต่อ …. WeVis ซึ่งเป็น… เริ่มสร้างเว็บไซต์นี้โดยมีจุดประสงค์เพื่อ…
+			และได้รับการสนันบสนุนจาก….
+		</div>
+	</div>
+	<div>
+		<div class="font-semibold">Open Source License</div>
+		<div>
+			(not final content) This website and its projects are open source. You're free to use, modify,
+			and share everything here. Attribution is appreciated but not required. Details in the
+			project-specific licenses. Link
+		</div>
+	</div>
 </div>
 <div class="bg-black py-8 px-16 text-white">
 	<div
