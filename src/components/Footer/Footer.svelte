4b9075c61f30c6cc0f02791119b3acd813a93c6e
Yuttakhan B <yuttakhanbaingen@gmail.com>
2023-10-07 19:34:08 +0700

    feat(wevisdemo#4): add layout of Footer

--- src/components/Footer/Footer.svelte
+++ src/components/Footer/Footer.svelte
@@ -1,1 +1,23 @@
-Footer
+<div class="bg-white py-8 px-16">
+	<div>เว็บไซต์นี้สร้างโดย</div>
+	<div>WeVis</div>
+	<div>ซึ่งเป็น xxxx</div>
+	<div>ความถูกต้อง</div>
+	<div>Open source license</div>
+</div>
+<div class="bg-black py-8 px-16 text-white">
+	<div
+		class="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-5 pb-4 border-solid border-0 border-b border-white"
+	>
+		<div class="row-span-2">logo</div>
+		<div>สมาชิกรัฐสภา</div>
+		<div>สมาชิกรัฐสภา</div>
+		<div>การลงมติ</div>
+		<div>เกี่ยวกับเรา</div>
+		<div>ติดต่อ</div>
+	</div>
+	<div class="flex justify-between mt-4">
+		<div>@ Parliament Watch 2023</div>
+		<div>นโยบายความปลอดภัย | Privacy Policy</div>
+	</div>
+</div>
