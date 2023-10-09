6d708966e00ac2d8d9878129919e0cfca1d1547e
KodaiCoder <nutdeknon@gmail.com>
2023-10-09 12:24:40 +0700

    feat: initial SideMenu & SideNav

--- src/components/NavigationBar/NavigationBar.svelte
+++ src/components/NavigationBar/NavigationBar.svelte
@@ -1,24 +1,52 @@
 <script lang="ts">
-	import Menu from 'carbon-icons-svelte/lib/Menu.svelte';
-	import Close from 'carbon-icons-svelte/lib/Close.svelte';
+	import { TestTool } from 'carbon-icons-svelte';
+	import SideMenu from './SideMenu.svelte';
+	import SideNav from './SideNav.svelte';
+	import PoliticianIcon from '$components/icons/PoliticianIcon.svelte';
+	import LawIcon from '$components/icons/LawIcon.svelte';
+	import VoteIcon from '$components/icons/VoteIcon.svelte';
 
 	let sideNavActive = false;
-	$: console.log(sideNavActive);
+	let screenSize: number;
+	$: if (screenSize > 1056) sideNavActive = false;
+
+	let menuList = [
+		{
+			label: 'สมาชิกรัฐสภา',
+			icon: PoliticianIcon,
+			sub: [
+				{ label: 'สภาผู้แทนราษฎร', icon: '' },
+				{ label: 'วุฒิสภา', icon: '' }
+			]
+		},
+		{
+			label: 'การออกกฎหมาย',
+			icon: LawIcon,
+			sub: [
+				{ label: 'กฎหมายในกระบวนการ', icon: '' },
+				{ label: 'รัฐออกกฎหมายอย่างไร', icon: '' }
+			]
+		},
+		{
+			label: 'การลงมติ',
+			icon: VoteIcon
+		},
+		{
+			label: 'เกี่ยวกับเรา',
+			icon: '',
+			sub: [
+				{ label: 'ที่มาของโครงการ', icon: '' },
+				{ label: 'ข้อมูลในเว็บนี้', icon: '' },
+				{ label: 'เกี่ยวกับ WeVis', icon: '' }
+			]
+		}
+	];
 </script>
 
-<header class="flex sticky items-center justify-between h-12 px-8 bg-[#161616]">
+<svelte:window bind:innerWidth={screenSize} />
+<header class="flex sticky items-center justify-between h-12 px-6 lg:px-10 bg-gray-100 z-20">
 	<div class="leading flex">
-		<button
-			class="flex items-center justify-center bg-[#161616] border-0 mr-4 cursor-pointer lg:hidden"
-			on:click={() => (sideNavActive = !sideNavActive)}
-		>
-			{#if !sideNavActive}
-				<Menu fill="white" size={24} aria-label="Open Menu" />
-			{:else}
-				<Close fill="white" size={24} aria-label="Close Menu" />
-			{/if}
-		</button>
-
+		<SideMenu isActive={sideNavActive} on:click={() => (sideNavActive = !sideNavActive)} />
 		<a href="/">
 			<picture class="flex items-center h-full">
 				<source media="(min-width: 1056px)" srcset="/images/logo/pw-long-white.png" height="18" />
@@ -29,3 +57,28 @@
 	<div class="nav" />
 	<div class="trailing" />
 </header>
+<SideNav isActive={sideNavActive}>
+	<ul>
+		{#each menuList as menu}
+			<div class="flex flex-col">
+				<li class="font-gray-10 p-2">{menu.label}</li>
+				{#if menu.sub}
+					<div class="pl-8">
+						<ul>
+							{#each menu.sub as sub, idx}
+								{#if idx < menu.sub.length - 1}
+									<hr class="  border-0 border-b p-0 m-0 border-gray-30/20 z-40" />
+								{/if}
+								<li class=" font-extralight font-gray-30 p-2">{sub.label}</li>
+								{#if idx < menu.sub.length - 1}
+									<hr class=" border-0 border-b p-0 m-0 border-gray-30/20 z-40" />
+								{/if}
+							{/each}
+						</ul>
+					</div>
+				{/if}
+			</div>
+			<hr class=" border-0 border-b p-0 m-0 border-gray-30/20 z-40" />
+		{/each}
+	</ul>
+</SideNav>
---
+++ src/components/NavigationBar/SideNav.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let isActive = false;
+</script>
+
+<aside
+	class="absolute top-0 left-0 h-screen pt-12 text-white bg-gray-80 z-10 w-80 opacity-100 transition-all duration-200 overflow-hidden"
+	class:!w-0={!isActive}
+	class:opacity-0={!isActive}
+>
+	<div class="pt-4">
+		<slot />
+	</div>
+</aside>
+
+<div
+	class="absolute top-0 left-0 h-screen backdrop-brightness-125 bg-white/50 w-full z-0"
+	class:hidden={!isActive}
+/>
