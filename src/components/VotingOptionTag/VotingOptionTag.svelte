d36dab9d7a24706ffe689a757c580b7871e99fca
Icyscools <woramat.ng@gmail.com>
2023-10-09 19:44:05 +0700

    feat(#13): use switch case

--- src/components/VotingOptionTag/VotingOptionTag.svelte
+++ src/components/VotingOptionTag/VotingOptionTag.svelte
@@ -11,34 +11,32 @@
 	let tagBgOpacity = 1;
 	let label = '';
 
-	if (typeof voteOption === 'string') {
-		switch (voteOption as DefaultVoteOption) {
-			case DefaultVoteOption.Agreed:
-				label = DefaultVoteOption.Agreed;
-				tagColorClass = 'bg-teal-50';
-				break;
-			case DefaultVoteOption.Disagreed:
-				label = DefaultVoteOption.Disagreed;
-				tagColorClass = 'bg-red-50 text-white';
-				break;
-			case DefaultVoteOption.Novote:
-				label = DefaultVoteOption.Novote;
-				tagColorClass = 'bg-gray-80 text-white';
-				break;
-			case DefaultVoteOption.Abstain:
-				label = DefaultVoteOption.Abstain;
-				tagColorClass = 'bg-gray-50 text-white';
-				break;
-			case DefaultVoteOption.Absent:
-				label = DefaultVoteOption.Absent;
-				tagColorClass = 'bg-gray-20';
-				break;
-		}
-	} else {
-		tagColorClass = 'bg-purple-70';
-		tagBgOpacity = voteOption?.colorIntensity * 0.7 + 0.3 ?? 1;
-		if (tagBgOpacity > 0.45) tagColorClass += ' text-white';
-		label = voteOption?.label ?? '';
+	switch (voteOption) {
+		case DefaultVoteOption.Agreed:
+			label = DefaultVoteOption.Agreed;
+			tagColorClass = 'bg-teal-50';
+			break;
+		case DefaultVoteOption.Disagreed:
+			label = DefaultVoteOption.Disagreed;
+			tagColorClass = 'bg-red-50 text-white';
+			break;
+		case DefaultVoteOption.Novote:
+			label = DefaultVoteOption.Novote;
+			tagColorClass = 'bg-gray-80 text-white';
+			break;
+		case DefaultVoteOption.Abstain:
+			label = DefaultVoteOption.Abstain;
+			tagColorClass = 'bg-gray-50 text-white';
+			break;
+		case DefaultVoteOption.Absent:
+			label = DefaultVoteOption.Absent;
+			tagColorClass = 'bg-gray-20';
+			break;
+		default:
+			tagColorClass = 'bg-purple-70';
+			tagBgOpacity = voteOption?.colorIntensity * 0.7 + 0.3 ?? 1;
+			if (tagBgOpacity > 0.45) tagColorClass += ' text-white';
+			label = voteOption?.label ?? '';
 	}
 </script>
 
