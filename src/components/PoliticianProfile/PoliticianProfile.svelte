33a05c4b3bed338dcacfaf0431001894fb8e9f01
double <dolawat.wannapira@gmail.com>
2023-10-10 20:56:48 +0700

    feat(#6): prepare different class for large and small styles for PoliticianProfile component

--- src/components/PoliticianProfile/PoliticianProfile.svelte
+++ src/components/PoliticianProfile/PoliticianProfile.svelte
@@ -10,72 +10,43 @@
 	export let isLarge = false;
 
 	$: fullname = `${firstname} ${lastname}`;
+
+	$: imgSize = isLarge ? 64 : 40;
+	$: imgClass = isLarge ? 'w-[64px] h-[64px]' : 'w-[40px] h-[40px]';
+	$: titleClass = isLarge
+		? 'text-lg leading-[28px] font-bold font-serif'
+		: 'text-sm leading-[18px] underline mb-1';
+	$: subtitleClass = isLarge ? 'leading-[18px]' : 'text-xs leading-[16px]';
 </script>
 
-{#if isLarge}
-	<div class="p-2 font-sans">
-		<div class="flex gap-4">
-			<div class="w-[64px] h-[64px] relative">
-				<img
-					src={avatar}
-					alt={`${fullname}-${id}`}
-					class="w-full h-full rounded-full object-cover"
-					width="64"
-					height="64"
-					loading="lazy"
-					decoding="async"
-				/>
-				<div
-					class="w-[16px] h-[16px] absolute bottom-0 right-0 rounded-full border-solid border border-[#C0BFBE] bg-white overflow-hidden p-[2px] flex items-center justify-center"
-				>
-					<img
-						class="w-full h-full object-contain"
-						src={party.logo}
-						alt={party.name}
-						width="16"
-						height="16"
-						loading="lazy"
-						decoding="async"
-					/>
-				</div>
-			</div>
-			<div>
-				<p class="text-lg leading-[28px] text-text-01 font-bold font-serif">{fullname}</p>
-				<p class="leading-[18px] text-text-02">พรรค{party.name}</p>
-				<p class="leading-[18px] text-text-02">{role}</p>
-			</div>
-		</div>
-	</div>
-{:else}
-	<div class="p-2 font-sans flex gap-4">
-		<div class="w-[40px] h-[40px] relative">
+<div {id} class="p-2 font-sans flex gap-4">
+	<div class="{imgClass} relative">
+		<img
+			src={avatar}
+			alt={fullname}
+			class="w-full h-full rounded-full object-cover"
+			width={imgSize}
+			height={imgSize}
+			loading="lazy"
+			decoding="async"
+		/>
+		<div
+			class="w-[16px] h-[16px] absolute bottom-0 right-0 rounded-full border-solid border border-[#C0BFBE] bg-white overflow-hidden p-[2px] flex items-center justify-center"
+		>
 			<img
-				src={avatar}
-				alt={`${fullname}-${id}`}
-				class="w-full h-full rounded-full object-cover"
-				width="40"
-				height="40"
+				class="w-full h-full object-contain"
+				src={party.logo}
+				alt={party.name}
+				width="16"
+				height="16"
 				loading="lazy"
 				decoding="async"
 			/>
-			<div
-				class="w-[16px] h-[16px] absolute bottom-0 right-0 rounded-full border-solid border border-[#C0BFBE] bg-white overflow-hidden p-[2px] flex items-center justify-center"
-			>
-				<img
-					class="w-full h-full object-contain"
-					src={party.logo}
-					alt={party.name}
-					width="16"
-					height="16"
-					loading="lazy"
-					decoding="async"
-				/>
-			</div>
-		</div>
-		<div>
-			<p class="text-sm leading-[18px] text-text-01 underline mb-1">{fullname}</p>
-			<p class="text-xs leading-[16px] text-text-02">พรรค{party.name}</p>
-			<p class="text-xs leading-[16px] text-text-02">{role}</p>
 		</div>
 	</div>
-{/if}
+	<div>
+		<p class="text-text-01 {titleClass}">{fullname}</p>
+		<p class="text-text-02 {subtitleClass}">พรรค{party.name}</p>
+		<p class="text-text-02 {subtitleClass}">{role}</p>
+	</div>
+</div>
