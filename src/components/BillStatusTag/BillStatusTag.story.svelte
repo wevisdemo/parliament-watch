91065b4f635989c7ee0a130daf1744fe44ed6aa5
Patsagorn <patsagorn_y@outlook.com>
2023-10-10 18:11:00 +0700

    feat(#21): init new BillStatusTag

---
+++ src/components/BillStatusTag/BillStatusTag.story.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { Hst } from '@histoire/plugin-svelte';
+	import BillStatusTag from './BillStatusTag.svelte';
+	export let Hst: Hst;
+</script>
+
+<Hst.Story title="BillStatusTag">
+	<BillStatusTag />
+</Hst.Story>
