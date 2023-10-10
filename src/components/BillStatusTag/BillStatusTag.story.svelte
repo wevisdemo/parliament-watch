5700cba1a0efd6632d57e7308a6f149411ef254b
Patsagorn <patsagorn_y@outlook.com>
2023-10-10 19:14:10 +0700

    feat(#21): make it functional

--- src/components/BillStatusTag/BillStatusTag.story.svelte
+++ src/components/BillStatusTag/BillStatusTag.story.svelte
@@ -1,9 +1,31 @@
 <script lang="ts">
 	import type { Hst } from '@histoire/plugin-svelte';
 	import BillStatusTag from './BillStatusTag.svelte';
+	import { BillStatus } from '$models/bill';
 	export let Hst: Hst;
+
+	let isLarge = false;
 </script>
 
-<Hst.Story title="BillStatusTag">
-	<BillStatusTag />
+<Hst.Story
+	title="BillStatusTag"
+	layout={{
+		type: 'grid'
+	}}
+>
+	<Hst.Variant title="In progress">
+		<BillStatusTag {isLarge} status={BillStatus.InProgress} />
+	</Hst.Variant>
+	<Hst.Variant title="Success">
+		<BillStatusTag {isLarge} status={BillStatus.Success} />
+	</Hst.Variant>
+	<Hst.Variant title="Rejected">
+		<BillStatusTag {isLarge} status={BillStatus.Rejected} />
+	</Hst.Variant>
+	<Hst.Variant title="Merged">
+		<BillStatusTag {isLarge} status={BillStatus.Merged} />
+	</Hst.Variant>
+	<svelte:fragment slot="controls">
+		<Hst.Checkbox title="isLarge" bind:value={isLarge} />
+	</svelte:fragment>
 </Hst.Story>
--- src/components/BillStatusTag/BillStatusTag.svelte
+++ src/components/BillStatusTag/BillStatusTag.svelte
@@ -1,1 +1,28 @@
-BillStatusTag
+<script lang="ts">
+	import { BillStatus } from '$models/bill';
+	import { Tag } from 'carbon-components-svelte';
+
+	export let isLarge = false;
+	export let status: BillStatus;
+	let label = status;
+	let className = '';
+
+	switch (status) {
+		case BillStatus.InProgress:
+			className = 'bg-yellow-20 text-text-10';
+			break;
+		case BillStatus.Success:
+			className = 'bg-teal-80 text-text-04';
+			break;
+		case BillStatus.Rejected:
+			className = 'bg-red-80 text-text-04';
+			break;
+		case BillStatus.Merged:
+			className = 'bg-purple-80 text-text-04';
+			break;
+	}
+
+	export { className as class };
+</script>
+
+<Tag class=" {className}" size={isLarge ? 'default' : 'sm'}>{label}</Tag>
