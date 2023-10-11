ebe29fe60fd122a6e8cae34539eefa0cdea2272d
Anner <icetherockth@gmail.com>
2023-10-11 21:54:04 +0700

    feat(#22): initialize function and make it functional

---
+++ src/components/BillCategoryTag/BillCategoryTag.story.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { Hst } from '@histoire/plugin-svelte';
+	import BillCategoryTag from './BillCategoryTag.svelte';
+	export let Hst: Hst;
+
+	let isLarge = false;
+</script>
+
+<Hst.Story title="BillCategoryTag" layout={{ type: 'grid' }}>
+	<Hst.Variant title="BillCategory">
+		<BillCategoryTag {isLarge} />
+	</Hst.Variant>
+	<svelte:fragment slot="controls">
+		<Hst.Checkbox title="isLarge" bind:value={isLarge} />
+	</svelte:fragment>
+</Hst.Story>
---
+++ src/components/BillCategoryTag/BillCategoryTag.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { Tag } from 'carbon-components-svelte';
+
+	export let isLarge = false;
+	let label = 'ขนส่งสาธารณะ';
+	let tagStyle = 'text-text-10 border-solid bg-white m-0 whitespace-nowrap';
+
+	let className = '';
+	export { className as class };
+</script>
+
+<Tag
+	class="{tagStyle} font-{isLarge ? 'semibold' : 'small'} {className}"
+	style="--tw-bg-opacity: 0; border-width: 1px;"
+	size={isLarge ? 'default' : 'sm'}>{label}</Tag
+>
