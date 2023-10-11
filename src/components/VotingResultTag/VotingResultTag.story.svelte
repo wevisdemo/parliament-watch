d6af17c82cffc33007e27ce7f06e3accbace02b9
Th1nkK1D <witheep@gmail.com>
2023-10-11 17:42:18 +0700

    docs(#12): split varient by result instead of isLarge

--- src/components/VotingResultTag/VotingResultTag.story.svelte
+++ src/components/VotingResultTag/VotingResultTag.story.svelte
@@ -2,34 +2,30 @@
 	import type { Hst } from '@histoire/plugin-svelte';
 	import VotingResultTag from './VotingResultTag.svelte';
 	import { DefaultVotingResult } from '$models/voting';
-	export let Hst: Hst;
-	let selectedResult = DefaultVotingResult.Passed as DefaultVotingResult | 'custom';
-	let customResult = '[ชื่อแคนดิเดต]ชนะ';
-	let result: DefaultVotingResult | string;
 
-	$: result = selectedResult === 'custom' ? customResult : selectedResult;
+	export let Hst: Hst;
+
+	let isLarge = false;
+	let customResult = '[ชื่อแคนดิเดต]ชนะ';
 </script>
 
 <Hst.Story title="VotingResultTag" layout={{ type: 'grid' }}>
-	<Hst.Variant title="Small (Default)">
-		<VotingResultTag {result} />
+	<Hst.Variant title="DefaultVotingResult.Passed">
+		<VotingResultTag result={DefaultVotingResult.Passed} {isLarge} />
 	</Hst.Variant>
-	<Hst.Variant title="Large">
-		<VotingResultTag {result} isLarge />
+
+	<Hst.Variant title="DefaultVotingResult.Failed">
+		<VotingResultTag result={DefaultVotingResult.Failed} {isLarge} />
+	</Hst.Variant>
+
+	<Hst.Variant title="CustomVotingResult">
+		<VotingResultTag result={customResult} {isLarge} />
+		<svelte:fragment slot="controls">
+			<Hst.Text bind:value={customResult} title="Custom result" />
+		</svelte:fragment>
 	</Hst.Variant>
 
 	<svelte:fragment slot="controls">
-		<Hst.Select
-			bind:value={selectedResult}
-			title="Result"
-			options={[
-				{ label: 'Passed', value: DefaultVotingResult.Passed },
-				{ label: 'Failed', value: DefaultVotingResult.Failed },
-				{ label: 'Custom', value: 'custom' }
-			]}
-		/>
-		{#if selectedResult === 'custom'}
-			<Hst.Text bind:value={customResult} title="Custom result" />
-		{/if}</svelte:fragment
-	>
+		<Hst.Checkbox bind:value={isLarge} title="isLarge" />
+	</svelte:fragment>
 </Hst.Story>
