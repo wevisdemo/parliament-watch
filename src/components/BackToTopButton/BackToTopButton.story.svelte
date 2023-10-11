cd832158609d0f6aa6740406e3b7aa825689db9c
Natthaphong Ruengpanyawut <tee4cute@gmail.com>
2023-10-11 11:32:47 +0700

    feat(#7): add back to top button component (#9)
    * feat(#7): just checked out and newly created component
    
    * feat(#7): ready for PR!
    
    * feat(#7): add 'bgColor' prop
    
    * feat(#7): clean up some code
    
    * Delete package-lock.json
    
    * feat(#7): edit code according to PR comments
    
    ---------
    
    Co-authored-by: Withee Poositasai <witheep@gmail.com>

---
+++ src/components/BackToTopButton/BackToTopButton.story.svelte
@@ -0,0 +1,120 @@
+<script lang="ts">
+	import type { Hst } from '@histoire/plugin-svelte';
+	import BackToTopButton from './BackToTopButton.svelte';
+	export let Hst: Hst;
+</script>
+
+<Hst.Story title="BackToTopButton">
+	<BackToTopButton
+		size={40}
+		padding={12}
+		border={1}
+		borderRadius={2}
+		color=""
+		bgColor=""
+		margin={20}
+		showAt={50}
+	/>
+	<pre style="background-color: rgba(0, 0, 0, 0.1);">
+		P
+		l
+		e
+		a
+		s
+		e
+
+		.
+
+		s
+		c
+		r
+		o
+		l
+		l
+
+		.
+
+		d
+		o
+		w
+		n
+
+		.
+
+		t
+		o
+
+		.
+
+		l
+		e
+		t
+
+		.
+
+		t
+		h
+		e
+
+		.
+
+		'BackToTopButton'
+		Component
+
+		.
+
+		s
+		h
+		o
+		w
+		n
+
+		.
+		.
+		.
+
+		Not yet?
+
+		.
+		.
+		.
+
+		.
+		.
+		.
+
+		.
+		.
+		.
+
+		.
+		.
+		.
+
+		Have you reached there?!?
+
+		.
+		.
+		.
+
+		.
+		.
+		.
+
+		A little bit more ...
+
+		.
+		.
+		.
+
+		.
+		.
+		.
+		
+		.
+		.
+		.
+
+		Wholaaaa! Click on the button to see the magic!
+	</pre>
+</Hst.Story>
