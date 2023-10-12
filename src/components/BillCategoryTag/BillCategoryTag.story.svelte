4f60c74cda49d32f2ff1b2452ccdbd584953f340
Anner <icetherockth@gmail.com>
2023-10-12 15:55:59 +0700

    feat(#22): update story

--- src/components/BillCategoryTag/BillCategoryTag.story.svelte
+++ src/components/BillCategoryTag/BillCategoryTag.story.svelte
@@ -3,14 +3,14 @@
 	import BillCategoryTag from './BillCategoryTag.svelte';
 	export let Hst: Hst;
 
+	let label = 'ขนส่งสาธารณะ';
 	let isLarge = false;
 </script>
 
-<Hst.Story title="BillCategoryTag" layout={{ type: 'grid' }}>
-	<Hst.Variant title="PublicTransport">
-		<BillCategoryTag {isLarge} label="ขนส่งสาธารณะ" />
-	</Hst.Variant>
+<Hst.Story title="BillCategoryTag">
+	<BillCategoryTag {isLarge} {label} />
 	<svelte:fragment slot="controls">
+		<Hst.Text title="Label" bind:value={label} />
 		<Hst.Checkbox title="isLarge" bind:value={isLarge} />
 	</svelte:fragment>
 </Hst.Story>
