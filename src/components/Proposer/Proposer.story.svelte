d69979eba3f8d97e0d7feab224fd952f2fff0d82
Yuttakhan B <yuttakhanbaingen@gmail.com>
2023-10-13 10:04:45 +0700

    feat(wevisdemo#29): add assembly and common prop to Proposer

--- src/components/Proposer/Proposer.story.svelte
+++ src/components/Proposer/Proposer.story.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
 	import type { Hst } from '@histoire/plugin-svelte';
 	import Proposer from './Proposer.svelte';
+	import { rep26 } from '../../mocks/data/assembly';
+
 	export let Hst: Hst;
 </script>
 
 <Hst.Story title="Proposer">
-	<Proposer />
+	<Proposer assembly={rep26} />
+	<Proposer
+		common={{
+			name: 'นายยิ่งชีพ อัชชานนท',
+			description: 'และประชาชน 200,000 คน'
+		}}
+	/>
 </Hst.Story>
--- src/components/Proposer/Proposer.svelte
+++ src/components/Proposer/Proposer.svelte
@@ -1,17 +1,59 @@
 <script lang="ts">
+	import PeopleIcon from '$components/icons/PeopleIcon.svelte';
+	import PoliticianIcon from '$components/icons/PoliticianIcon.svelte';
+	import type { Assembly } from '$models/assembly';
+	import dayjs from 'dayjs';
+
+	type CommonProposer = {
+		name: string;
+		description?: string;
+	};
+
+	export let assembly: Assembly | undefined = undefined;
+	export let common: CommonProposer | undefined = undefined;
+
+	$: proposerName = () => {
+		if (assembly) {
+			return assembly.name;
+		} else if (common) {
+			return common.name;
+		}
+
+		return '';
+	};
+
+	$: proposerTerm = () => {
+		if (assembly) {
+			return `ชุดที่ ${assembly.term} (${dayjs(assembly.startedAt).year()})`;
+		}
+
+		return '';
+	};
 </script>
 
-<div class="flex flex-col sm:flex-row sm:items-center">
+<div class="flex flex-col sm:flex-row items-start sm:items-center">
 	<div class="w-6 h-6">
-		<img
+		<!-- <img
 			class="w-full h-full rounded-full object-cover"
 			loading="lazy"
 			decoding="async"
 			alt="proposer avatar"
-		/>
+		/> -->
+		{#if assembly}
+			<div class="flex items-center justify-center w-full h-full rounded-full bg-black">
+				<PoliticianIcon class="text-white" />
+			</div>
+		{/if}
+		{#if common}
+			<div class="flex items-center justify-center w-full h-full rounded-full bg-black">
+				<PeopleIcon class="text-white" />
+			</div>
+		{/if}
 	</div>
 	<div class="mt-1 sm:ml-1 break-words">
-		<span>very long name</span>
-		<span>description </span>
+		<span class="text-text-01">{proposerName()} {proposerTerm()}</span>
+		{#if common?.description}
+			<span class="text-text-02">{common.description}</span>
+		{/if}
 	</div>
 </div>
