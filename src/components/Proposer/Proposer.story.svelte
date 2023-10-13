c821190429e232513c46bef0fc0969cb02ac7afd
Yuttakhan B <yuttakhanbaingen@gmail.com>
2023-10-13 08:31:43 +0700

    feat(wevisdemo#29): initialize Proposer component

---
+++ src/components/Proposer/Proposer.story.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { Hst } from '@histoire/plugin-svelte';
+	import Proposer from './Proposer.svelte';
+	export let Hst: Hst;
+</script>
+
+<Hst.Story title="Proposer">
+	<Proposer />
+</Hst.Story>
