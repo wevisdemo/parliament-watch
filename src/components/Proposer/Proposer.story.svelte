3679cba9be2e8849a993193d35a10787f086ba46
Yuttakhan B <yuttakhanbaingen@gmail.com>
2023-10-13 10:47:39 +0700

    feat(wevisdemo#29): bundle party and politician as prop, add example to historia

--- src/components/Proposer/Proposer.story.svelte
+++ src/components/Proposer/Proposer.story.svelte
@@ -2,11 +2,19 @@
 	import type { Hst } from '@histoire/plugin-svelte';
 	import Proposer from './Proposer.svelte';
 	import { rep26 } from '../../mocks/data/assembly';
+	import { movingForwardPolitician } from '../../mocks/data/politician';
+	import { movingForwardParty } from '../../mocks/data/party';
 
 	export let Hst: Hst;
 </script>
 
 <Hst.Story title="Proposer">
+	<Proposer
+		partyPolitician={{
+			politician: movingForwardPolitician,
+			party: movingForwardParty
+		}}
+	/>
 	<Proposer assembly={rep26} />
 	<Proposer
 		common={{
--- src/components/Proposer/Proposer.svelte
+++ src/components/Proposer/Proposer.svelte
@@ -2,6 +2,7 @@
 	import PeopleIcon from '$components/icons/PeopleIcon.svelte';
 	import PoliticianIcon from '$components/icons/PoliticianIcon.svelte';
 	import type { Assembly } from '$models/assembly';
+	import type { Party } from '$models/party';
 	import type { Politician } from '$models/politician';
 	import dayjs from 'dayjs';
 
@@ -10,12 +11,18 @@
 		description?: string;
 	};
 
-	export let politician: Politician | undefined = undefined;
+	type PartyPolitician = {
+		politician: Politician;
+		party: Party;
+	};
+
+	export let partyPolitician: PartyPolitician | undefined = undefined;
 	export let assembly: Assembly | undefined = undefined;
 	export let common: CommonProposer | undefined = undefined;
 
 	$: proposerName = () => {
-		if (politician) {
+		if (partyPolitician) {
+			const { politician } = partyPolitician;
 			return [politician.prefix ?? '', politician.firstname, politician.lastname].join(' ');
 		} else if (assembly) {
 			return assembly.name;
@@ -35,29 +42,33 @@
 	};
 </script>
 
-<div class="flex flex-col sm:flex-row items-start sm:items-center">
-	<div class="w-6 h-6">
-		<!-- <img
-			class="w-full h-full rounded-full object-cover"
-			loading="lazy"
-			decoding="async"
-			alt="proposer avatar"
-		/> -->
-		{#if assembly}
+<div class="flex flex-col sm:flex-row justify-start items-start">
+	<div class="w-6 h-6 mb-1">
+		{#if partyPolitician}
+			<img
+				class="w-full h-full rounded-full object-cover"
+				loading="lazy"
+				decoding="async"
+				alt={`${partyPolitician.politician.firstname} ${partyPolitician.politician.lastname}`}
+				src={partyPolitician.politician.avatar}
+			/>
+		{:else if assembly}
 			<div class="flex items-center justify-center w-full h-full rounded-full bg-black">
 				<PoliticianIcon class="text-white" />
 			</div>
-		{/if}
-		{#if common}
+		{:else if common}
 			<div class="flex items-center justify-center w-full h-full rounded-full bg-black">
 				<PeopleIcon class="text-white" />
 			</div>
 		{/if}
 	</div>
-	<div class="mt-1 sm:ml-1 break-words">
+	<div class="sm:ml-1 break-words">
 		<span class="text-text-01">{proposerName()} {proposerTerm()}</span>
 		{#if common?.description}
 			<span class="text-text-02">{common.description}</span>
+		{/if}
+		{#if partyPolitician?.party.name}
+			<br /><span class="text-text-02">พรรค{partyPolitician?.party.name}</span>
 		{/if}
 	</div>
 </div>
