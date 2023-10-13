9fa37c908f29779537af6b782aae4101032662b7
Yuttakhan B <yuttakhanbaingen@gmail.com>
2023-10-13 09:09:57 +0700

    feat(wevisdemo#29): add layout of Proposer component

--- src/components/Proposer/Proposer.svelte
+++ src/components/Proposer/Proposer.svelte
@@ -1,1 +1,17 @@
-Proposer
+<script lang="ts">
+</script>
+
+<div class="flex flex-col sm:flex-row sm:items-center">
+	<div class="w-6 h-6">
+		<img
+			class="w-full h-full rounded-full object-cover"
+			loading="lazy"
+			decoding="async"
+			alt="proposer avatar"
+		/>
+	</div>
+	<div class="mt-1 sm:ml-1 break-words">
+		<span>very long name</span>
+		<span>description </span>
+	</div>
+</div>
