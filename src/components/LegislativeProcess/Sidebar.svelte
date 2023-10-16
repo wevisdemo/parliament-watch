92cbe036cf04183087eb12dcc918c008ffd61e0d
thasarito <thasarit.pysr@gmail.com>
2023-10-16 12:19:32 +0700

    feat: #28 create legislative process' page sidebar component

---
+++ src/components/LegislativeProcess/Sidebar.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import { SideNav, SideNavItems, SideNavMenu, SideNavMenuItem } from 'carbon-components-svelte';
+	import { onMount } from 'svelte';
+
+	let sections = [
+		{
+			menu: 'แนะนำภาพรวม',
+			menuItem: [
+				{ text: 'กฎหมายสำเร็จได้อย่างไร' },
+				{ text: 'กฎหมายในเว็บไซต์นี้' },
+				{ text: 'ประเภทของกฎหมาย' },
+				{ text: 'สถานะของกฎหมาย' }
+			]
+		},
+		{
+			menu: 'ขั้นตอนของกฎหมาย',
+			menuItem: [
+				{ text: 'ที่มาของกฎหมาย' },
+				{ text: 'ขั้นตอนโดยย่อ' },
+				{ text: 'ขั้นตอนโดยละเอียด' }
+			]
+		},
+		{
+			menu: 'ประเภทการลงมติ',
+			menuItem: [{ text: 'ประเภทของมติ' }, { text: 'ผลของการลงมติ' }, { text: 'การลงมติอื่นๆ' }]
+		},
+		{
+			menu: 'ส.ส. และ ส.ว. ทำอะไรบ้างในสภา',
+			menuItem: [
+				{ text: 'หน้าที่ขออง ส.ส.' },
+				{ text: 'หน้าที่ของ ส.ว.' },
+				{ text: 'หน้าที่ของสภาร่วม (ส.ส. + ส.ว.)' }
+			]
+		}
+	];
+</script>
+
+<SideNav isOpen={true} fixed={false} class="relative">
+	<SideNavItems>
+		{#each sections as section}
+			<SideNavMenu text={section.menu} expanded class="font-bold text-black">
+				{#each section.menuItem as item}
+					<SideNavMenuItem href="/legislative-process/{item.text}">{item.text}</SideNavMenuItem>
+				{/each}
+			</SideNavMenu>
+		{/each}
+	</SideNavItems>
+</SideNav>
