fee3475608f3c71ab17b5584000f82644333b3e7
napatswift <napat.do@ku.th>
2023-10-21 00:36:15 +0800

    feat(#34): add SearchResult component

---
+++ src/components/SearchResult/SearchResult.story.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { Hst } from '@histoire/plugin-svelte';
+	import SearchResult from './SearchResult.svelte';
+	export let Hst: Hst;
+</script>
+
+<Hst.Story title="SearchResult">
+	<SearchResult />
+</Hst.Story>
---
+++ src/components/SearchResult/SearchResult.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import SearchResultGroup from '$components/SearchResultGroup/SearchResultGroup.svelte';
+	import VoteIcon from '$components/icons/VoteIcon.svelte';
+	import { BillStatus } from '$models/bill';
+	import type { SearchIndexes, SearchResultItem } from '$models/search';
+	import { DefaultVotingResult } from '$models/voting';
+
+	const searchIndexes: SearchIndexes = {
+		politicians: [
+			{
+				name: 'สุชาติ ชมกลิ่น',
+				description: 'สส.บัญชีรายชื่อ | รวมไทยสร้างชาติ'
+			},
+			{
+				name: 'สมคิด จาตุศรีพิทักษ์',
+				description: 'นายกรัฐมนตรี'
+			}
+		],
+
+		bills: [
+			{
+				name: 'ร่าง พ.ร.บ.สุราก้าวหน้า',
+				status: BillStatus.Rejected
+			},
+			{
+				name: 'ร่าง พ.ร.บ. งบประมาณรายจ่ายประจําปี 2566',
+				status: BillStatus.InProgress
+			}
+		],
+
+		votings: [
+			{
+				name: 'ร่าง พ.ร.บ. สุราก้าวหน้า (วาระที่ 1)',
+				result: DefaultVotingResult.Failed
+			},
+			{
+				name: 'ร่าง พ.ร.บ. แรงงานสัมพันธ์',
+				result: DefaultVotingResult.Passed
+			}
+		]
+	};
+
+	const resultItems: SearchResultItem[] = [
+		{
+			heading: 'สุชาติ ชมกลิ่น',
+			description: 'สส.บัญชีรายชื่อ | รวมไทยสร้างชาติ',
+			url: '/politician/1'
+		},
+		{
+			heading: 'สมคิด จาตุศรีพิทักษ์',
+			description: 'นายกรัฐมนตรี',
+			url: '/politician/2'
+		},
+		{
+			heading: 'ร่าง พ.ร.บ.สุราก้าวหน้า',
+			billStatus: BillStatus.InProgress,
+			url: '/bill/1'
+		},
+		{
+			heading: 'ร่าง พ.ร.บ. งบประมาณรายจ่ายประจําปี 2566',
+			voteResult: DefaultVotingResult.Failed,
+			url: '/bill/2'
+		}
+	];
+</script>
+
+<div>
+	<SearchResultGroup heading={'การลงมติ'} items={resultItems}>
+		<VoteIcon slot="icon" class="fill-interactive-01" />
+	</SearchResultGroup>
+</div>
