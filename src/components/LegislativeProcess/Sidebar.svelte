b2bcf3612606af857e56cc06b456a90f07f33def
thasarito <thasarit.pysr@gmail.com>
2023-10-21 09:12:35 +0700

    feat(#28): add Legislative Process Page Sidebar (#37)
    * feat: #28 create legislative process' page sidebar component
    
    * refactor: change let to const
    
    * style: place sidebar in legislative page in appropriate placce on both  mobile and desktop
    
    * feat: add id and link to each section
    
    * style: change scroll behavior to smooth
    
    * fix: typo
    
    Co-authored-by: Woramat Ngamkham <meet.ice1@gmail.com>
    
    * refactor: simplify string interpolation
    
    * style: remove overlay background
    
    * style: make css selector more specific
    
    * fix: prevent sidenav from collapse
    
    * style: fix styling on mobile to match figma
    
    ---------
    
    Co-authored-by: Woramat Ngamkham <meet.ice1@gmail.com>

---
+++ src/components/LegislativeProcess/Sidebar.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { SideNav, SideNavItems, SideNavMenu, SideNavMenuItem } from 'carbon-components-svelte';
+
+	const sections = [
+		{
+			menu: 'แนะนำภาพรวม',
+			menuItem: [
+				{ text: 'กฎหมายสำเร็จได้อย่างไร' },
+				{ text: 'กฎหมายในเว็บไซต์นี้' },
+				{ text: 'ประเภทของกฎหมาย' },
+				{ text: 'สถานะของกฎหมาย' }
+			]
+		},
+		{
+			menu: 'ขั้นตอนของกฎหมาย',
+			menuItem: [
+				{ text: 'ที่มาของกฎหมาย' },
+				{ text: 'ขั้นตอนโดยย่อ' },
+				{ text: 'ขั้นตอนโดยละเอียด' }
+			]
+		},
+		{
+			menu: 'ประเภทการลงมติ',
+			menuItem: [{ text: 'ประเภทของมติ' }, { text: 'ผลของการลงมติ' }, { text: 'การลงมติอื่นๆ' }]
+		},
+		{
+			menu: 'ส.ส. และ ส.ว. ทำอะไรบ้างในสภา',
+			menuItem: [
+				{ text: 'หน้าที่ของ ส.ส.' },
+				{ text: 'หน้าที่ของ ส.ว.' },
+				{ text: 'หน้าที่ของสภาร่วม (ส.ส. + ส.ว.)' }
+			]
+		}
+	];
+</script>
+
+<SideNav isOpen={true} fixed={true} class="relative z-0">
+	<SideNavItems>
+		{#each sections as section}
+			<SideNavMenu text={section.menu} expanded class="font-bold text-black">
+				{#each section.menuItem as item}
+					<SideNavMenuItem href="#{item.text}">{item.text}</SideNavMenuItem>
+				{/each}
+			</SideNavMenu>
+		{/each}
+	</SideNavItems>
+</SideNav>
