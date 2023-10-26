22ff66a04d9b93e5c7388ffd1c81825027220f93
napatswift <napat.do@ku.th>
2023-10-26 13:15:54 +0800

    feat(#34): implement navigationbar search function (#39)
    * feat(#34): add SearchResultGroup component
    
    * feat(#34): add SearchResult component
    
    * feat(#34): change styling and add a dispatcher
    
    * feat(#34): add search function
    
    * refactor(#34): update type definitions
    
    * feat(#34): add NoResultItemsFound component
    
    * feat(#34): show search results
    
    * feat(#34): hightlight text using component
    
    * feat(#34): render hightlighted text
    
    * docs(#34): update function document
    
    * refactor(#34): set text class as a prop
    
    * docs(#34): update component stories
    
    * fix(#34): use string literal
    
    * style(#34): remove or add space
    
    * refactor(#34): rename an argument's name and merge condition
    
    * feat: add unit test command
    
    * fix(#34): fix styling
    
    * feat(#34): add Thai word tokenizer
    
    * test(#34): add tests for word segment
    
    * style(#34): change function declaration and if condition

---
+++ src/components/SearchResultGroup/HightlightText.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { HighlightedText } from '$models/search';
+
+	export let textList: HighlightedText[];
+	export let textClass: string;
+	export let textHighlightClass = 'font-bold';
+</script>
+
+<span class="highlighted-text {textClass}">
+	{#each textList as text}
+		{#if text.highlight}
+			<span class={textHighlightClass}>{text.text}</span>
+		{:else}
+			<span>{text.text}</span>
+		{/if}
+	{/each}
+</span>
---
+++ src/components/SearchResultGroup/SearchResultGroup.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { SearchResultItem } from '$models/search';
+	import ResultItem from './ResultItem.svelte';
+
+	export let heading: string;
+	export let items: SearchResultItem[];
+</script>
+
+<div>
+	<div class="px-4 py-2 flex items-center gap-2 bg-ui-01">
+		<slot name="icon" />
+		<span class="text-xs">{heading}</span>
+	</div>
+	{#each items as item}
+		<ResultItem {item} />
+	{/each}
+</div>
