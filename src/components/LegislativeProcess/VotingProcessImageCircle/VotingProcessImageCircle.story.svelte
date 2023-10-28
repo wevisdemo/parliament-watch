b4e7ad4b18bba1b7ad3c83051b3f535262fabaec
KodaiCoder <nutdeknon@gmail.com>
2023-10-28 22:35:45 +0700

    feat(#46): adding content feat(#46): adding VotingProcessImageCircle as reuseable component docs(#46): adding Histoire forVotingProcessImageCircle component

---
+++ src/components/LegislativeProcess/VotingProcessImageCircle/VotingProcessImageCircle.story.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { Hst } from '@histoire/plugin-svelte';
+	import VotingProcessImageCircle from './VotingProcessImageCircle.svelte';
+	export let Hst: Hst;
+
+	const icon = '/images/logo/pw-icon-white.png';
+</script>
+
+<Hst.Story title="VotingProcessImageCircle" layout={{ type: 'grid', width: 200 }}>
+	<Hst.Variant title="No image source">
+		<VotingProcessImageCircle title="title" description="description" />
+	</Hst.Variant>
+	<Hst.Variant title="With image source">
+		<VotingProcessImageCircle
+			title="title"
+			description="description"
+			src={icon}
+			alt={'test alt text'}
+		/>
+	</Hst.Variant>
+</Hst.Story>
---
+++ src/components/LegislativeProcess/VotingProcessImageCircle/VotingProcessImageCircle.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	export let src = '';
+	export let alt = '';
+	export let title: string;
+	export let description = '';
+</script>
+
+<div class="flex-1 max-w-[280px]">
+	<div class="mb-2 flex justify-center text-center">
+		{#if src}
+			<img class="w-32 h-32 rounded-full" {src} alt={alt ? `${alt}` : `${title}`} />
+		{:else}
+			<div class="bg-ui-01 w-32 h-32 rounded-full flex justify-center items-center">
+				<p class="text-text-03">
+					image<br />
+					coming soon
+				</p>
+			</div>
+		{/if}
+	</div>
+	<div class="flex flex-col items-center text-center">
+		<p><strong>{title}</strong></p>
+		<p>{description}</p>
+	</div>
+</div>
