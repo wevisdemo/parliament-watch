dae1e4144e049d17ad506288ca017a5668bfcaba
PopoTeaTree <https://github.com/PopoTeaTree>
2023-10-30 23:32:25 +0700

    fix(component): comparison table style and variable name

--- src/components/ComparisonTable/ComparisonTable.svelte
+++ src/components/ComparisonTable/ComparisonTable.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-	export let tableData: { [key: string]: string }[];
+	export let proposerComparisonTableData: { [key: string]: string }[];
 </script>
 
-{#if tableData.length > 0}
+{#if proposerComparisonTableData.length > 0}
 	<table class="w-full text-sm">
 		<thead class="bg-gray-20">
 			<tr class="w-full h-12 border-b-2">
-				{#each Object.keys(tableData[0]) as columnHeading}
+				{#each Object.keys(proposerComparisonTableData[0]) as columnHeading}
 					<th class="text-01 font-semibold px-4 p-4">{columnHeading}</th>
 				{/each}
 			</tr>
 		</thead>
 		<tbody class="w-full bg-gray-10">
-			{#each Object.values(tableData) as row}
+			{#each Object.values(proposerComparisonTableData) as row}
 				<tr class="w-full h-12">
 					{#each Object.values(row) as cell}
 						<td class="p-4 align-middle">{cell}</td>
@@ -23,7 +23,7 @@
 	</table>
 {/if}
 
-<style lang="postcss">
+<style lang="ts">
 	td:first-child {
 		@apply font-bold;
 	}
