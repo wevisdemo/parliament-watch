2ea86827bd5c5522ee49cf24138820984dd6b069
Nutchapon Makelai <nutdeknon@gmail.com>
2023-10-31 10:56:26 +0700

    feat(#46): add voting process in legislative process page (#57)
    * fix(#35): fix NavigationBar overflow
    fix(#35): fix flickering effect of slide animation
    
    * fix(#35): remove h-12 from main layout
    refactor(#35): adding h-12 to header tag
    refactor(#35): made NavigationPane have fixed position
    
    * feat(#46): adding content
    feat(#46): adding VotingProcessImageCircle as reuseable component
    docs(#46): adding Histoire forVotingProcessImageCircle component
    
    * fix(#46): fix following comment
    - removing changing in-line conditional in alt attribute
    - using placehold.co url as src of placeholder image
    - removing Histoire for VotingProcessImageCircle component
    
    * feat(#46): adding lazy load for img tag
    
    ---------
    
    Co-authored-by: Withee Poositasai <witheep@gmail.com>

--- src/routes/legislative-process/+page.svelte
+++ src/routes/legislative-process/+page.svelte
@@ -3,6 +3,7 @@
 	import buddhistEra from 'dayjs/plugin/buddhistEra';
 	import 'dayjs/locale/th';
 	import Sidebar from '$components/LegislativeProcess/Sidebar.svelte';
+	import VotingProcessImageCircle from '$components/LegislativeProcess/VotingProcessImageCircle.svelte';
 	import ComparisonTable from '$components/ComparisonTable/ComparisonTable.svelte';
 	import ProcessCard from '$components/LegislativeProcess/ProcessCard.svelte';
 	import ProcessCardArrow from '$components/LegislativeProcess/ProcessCardArrow.svelte';
@@ -298,16 +299,101 @@
 				<h1 class="fluid-heading-05" id="เข้าใจการลงมติของ สส. และ สว.">
 					เข้าใจการลงมติของ สส. และ สว.
 				</h1>
-				<div>
+				<div class="mb-6">
 					<h2 class="fluid-heading-04" id="การลงมติคืออะไร เกิดขึ้นในขั้นตอนไหนบ้าง ?">
 						การลงมติคืออะไร เกิดขึ้นในขั้นตอนไหนบ้าง ?
 					</h2>
 					<hr />
+					<div class="mt-6">
+						<p>
+							การลงมติ เป็นส่วนหนึ่งของการประชุมสภา ใช้ในกระบวนการพิจารณาเรื่องต่างๆ
+							ทั้งในสภาผู้แทนราษฎร และวุฒิสภา เช่น การพิจารณาญัตติ, การเสนอและพิจารณาร่างกฎหมาย,
+							การพิจารณาให้ความเห็นชอบนายกรัฐมนตรี, การพิจารณาในขั้นกรรมาธิการ เป็นต้น
+							โดยมติของสภาจะกระทำโดยการออกเสียงลงคะแนน ตามวิธีการที่กำหนดไว้ในข้อบังคับการประชุม
+							สมาชิกรัฐสภาหนึ่งคนมีสิทธิออกเสียงได้หนึ่งเสียง และมติของสภาให้เป็นไปตามเสียงข้างมาก
+							เพื่อให้ได้ข้อยุติของปัญหาและเพื่อให้ได้แนวทางที่สภาจะต้องดำเนินการต่อไป
+						</p>
+					</div>
 				</div>
-				<div>
-					<h2 class="fluid-heading-04" id="วิธีการลงมติ">วิธีการลงมติ</h2>
-					<hr />
+				<div class="my-6 grid gap-4">
+					<div>
+						<h2 class="fluid-heading-04" id="วิธีการลงมติ">วิธีการลงมติ</h2>
+						<hr />
+						<div class="mt-6">
+							<p>การลงมติในสภากระทำผ่านการออกเสียงลงคะแนน ซึ่งแบ่งเป็น 2 กรณี</p>
+						</div>
+					</div>
+					<div class="grid gap-4">
+						<p><strong>1. การออกเสียงลงคะแนนเปิดเผย</strong></p>
+						<div class="flex gap-3 justify-center">
+							<VotingProcessImageCircle
+								src="https://placehold.co/120"
+								title="การลงคะแนนเสียงด้วยวาจา"
+								description="ผ่านการเรียกชื่อสมาชิกตามหมายเลขประจำตัว เช่น การโหวตเลือกนายกรัฐมนตรี"
+							/>
+							<VotingProcessImageCircle
+								src="https://placehold.co/120"
+								title="การใช้เครื่องลงคะแนนเสียง"
+								description="เพื่อลงมติเห็นด้วยหรือไม่ต่อประเด็นต่างๆ"
+							/>
+						</div>
+						<p><strong>2. การออกเสียงลงคะแนนลับ</strong></p>
+						<div class="flex gap-3 justify-center">
+							<VotingProcessImageCircle
+								src="https://placehold.co/120"
+								title="การทำเครื่องหมายบนแผ่นกระดาษ"
+								description="ใส่ซองที่เจ้าหน้าที่จัดให้"
+							/>
+							<VotingProcessImageCircle
+								src="https://placehold.co/120"
+								title="การใช้เครื่องออกเสียงลงคะแนน"
+								description="ตามที่ประชุมกำหนด"
+							/>
+						</div>
+					</div>
+					<div class="grid gap-2">
+						<p><strong>เกณฑ์ในการเลือกวิธีการลงมติ</strong> ตามข้อบังคับการประชุมสภา</p>
+						<div class="lg:flex gap-4">
+							<div class="bg-ui-01 p-4 flex-1 mb-4 lg:mb-0 h-fit">
+								<div class="mb-3">
+									<p><strong>ที่ประชุมสภาผู้แทนฯ หรือที่ประชุมวุฒิสภา</strong></p>
+								</div>
+								<div>
+									<ul class="list-disc pl-6">
+										<li>การออกเสียงลงคะแนนให้กระทำเป็นการเปิดเผย</li>
+										<li>
+											ถ้ามีสมาชิกเสนอญัตติโดยมีผู้รับรองไม่น้อยกว่า 20 คนของสภานั้นๆ
+											ขอให้กระทำเป็นการลับ จึงให้ลงคะแนนลับ
+										</li>
+										<li>
+											แต่ถ้ามีผู้คัดค้านและมีผู้รับรองไม่น้อยกว่า 1 ใน 3 ของสมาชิกในที่ประชุม
+											ให้ถือเป็นเอกสิทธิ์ที่จะลงคะแนนโดยเปิดเผย
+										</li>
+									</ul>
+								</div>
+							</div>
+							<div class="bg-ui-01 p-4 flex-1 h-fit">
+								<div class="mb-3">
+									<p><strong>ประชุมสภาร่วม</strong></p>
+								</div>
+								<div>
+									<ul class="list-disc pl-6">
+										<li>การออกเสียงลงคะแนนให้กระทำเป็นการเปิดเผย</li>
+										<li>
+											ถ้าคณะรัฐมนตรีร้องขอ หรือสมาชิกรัฐสภาเสนอญัตติโดยมีสมาชิกรับรองไม่น้อยกว่า 40
+											คนขอให้กระทำเป็นการลับ จึงให้ลงคะแนนลับ
+										</li>
+										<li>
+											แต่ถ้ามีสมาชิกคัดค้านและมีผู้รับรองไม่น้อยกว่า 1 ใน 3
+											ของสมาชิกรัฐสภาในที่ประชุมให้ถือเป็นเอกสิทธิ์ที่จะลงคะแนนโดยเปิดเผย
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
+
 				<div>
 					<h2 class="fluid-heading-04" id="ประเภทการลงมติ">ประเภทการลงมติ</h2>
 					<hr />
