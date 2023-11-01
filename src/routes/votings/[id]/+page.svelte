fa522453e17399331e37e760b8b392067e883155
tesol2y090 <gliese090@gmail.com>
2023-11-01 22:30:47 +0700

    feat(wev-10): add breadcrumb

--- src/routes/votings/[id]/+page.svelte
+++ src/routes/votings/[id]/+page.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
+	import { Breadcrumb, BreadcrumbItem } from 'carbon-components-svelte';
+
 	export let data;
 </script>
 
-<div class="whitespace-pre">
-	{JSON.stringify(data, undefined, 2)}
+<div class="flex flex-col min-h-screen">
+	<Breadcrumb
+		noTrailingSlash
+		class="px-4 py-2 body-compact-01 [&>.bx--breadcrumb]:flex [&>.bx--breadcrumb]:flex-wrap"
+	>
+		<BreadcrumbItem href="/home">หน้าแรก</BreadcrumbItem>
+		<BreadcrumbItem href="/voting">การลงมติ</BreadcrumbItem>
+		<BreadcrumbItem>{data.voting.title}</BreadcrumbItem>
+	</Breadcrumb>
+	<div class="whitespace-pre">
+		{JSON.stringify(data, undefined, 2)}
+	</div>
 </div>
