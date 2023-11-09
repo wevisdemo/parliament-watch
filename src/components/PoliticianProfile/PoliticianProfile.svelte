07e1d5a5bb3ced7a147b58a252f203d00c9f2ec9
Th1nkK1D <witheep@gmail.com>
2023-11-09 14:58:21 +0700

    feat: add isActive props in Politician Profile

--- src/components/PoliticianProfile/PoliticianProfile.svelte
+++ src/components/PoliticianProfile/PoliticianProfile.svelte
@@ -5,12 +5,12 @@
 	export let firstname: string;
 	export let lastname: string;
 	export let avatar: string;
-	export let party: Party;
+	export let party: Pick<Party, 'logo' | 'name'>;
 	export let role: string;
 	export let isLarge = false;
+	export let isActive = true;
 
 	$: fullname = `${firstname} ${lastname}`;
-
 	$: imgSize = isLarge ? 64 : 40;
 	$: imgClass = isLarge ? 'w-[64px] h-[64px]' : 'w-[40px] h-[40px]';
 	$: titleClass = isLarge
@@ -19,12 +19,13 @@
 	$: subtitleClass = isLarge ? 'leading-[18px]' : 'text-xs leading-[16px]';
 </script>
 
-<div {id} class="p-2 font-sans flex gap-4">
+<!-- TODO: use id to link to politician page -->
+<div {id} class="p-2 font-sans flex gap-4" class:opacity-50={!isActive}>
 	<div class="{imgClass} relative">
 		<img
 			src={avatar}
 			alt={fullname}
-			class="w-full h-full rounded-full object-cover"
+			class="rounded-full object-cover"
 			width={imgSize}
 			height={imgSize}
 			loading="lazy"
@@ -34,7 +35,7 @@
 			class="w-[16px] h-[16px] absolute bottom-0 right-0 rounded-full border-solid border border-[#C0BFBE] bg-white overflow-hidden p-[2px] flex items-center justify-center"
 		>
 			<img
-				class="w-full h-full object-contain"
+				class="object-contain"
 				src={party.logo}
 				alt={party.name}
 				width="16"
@@ -44,7 +45,7 @@
 			/>
 		</div>
 	</div>
-	<div>
+	<div class="flex-1">
 		<p class="text-text-01 {titleClass}">{fullname}</p>
 		<p class="text-text-02 {subtitleClass}">พรรค{party.name}</p>
 		<p class="text-text-02 {subtitleClass}">{role}</p>
