b2c573a5b52d7992be5faa05d703145a63e25fe3
petchill <songpon111333@gmail.com>
2023-12-02 18:05:14 +0700

    feat(wev-14): add vote filter hide logic in responsive

--- src/components/Votes/VotesBody.svelte
+++ src/components/Votes/VotesBody.svelte
@@ -6,11 +6,14 @@
 		VoteSummary
 	} from '../../routes/assemblies/[id]/votes/+page';
 	import VotesFilter from './VotesFilter.svelte';
+	import VotesFilterModal from './VotesFilterModal.svelte';
 	import type { VotesFilter as IVotesFilter } from './shared';
 
 	export let votes: VoteSummary[] = [];
 	export let filterOptions: FilterOptions;
-	export let showFilter = true;
+	$: outerWidth = 769;
+	$: isMD = outerWidth > 768;
+	$: showFilter = isMD ? true : false;
 	let filter: IVotesFilter = {
 		name: '',
 		result: ['ผ่าน', 'ไม่ผ่าน'],
@@ -28,7 +31,16 @@
 	});
 </script>
 
+<svelte:window bind:outerWidth />
+
 <div class="flex bg-white flex flex-col md:flex-row justify-center">
-	<VotesFilter {votes} bind:filter bind:showFilter />
+	<section class="md:block hidden flex">
+		<VotesFilter {votes} bind:filter bind:showFilter />
+	</section>
 	<VotesTable votes={filteredVotes} bind:showFilter />
+	{#if !isMD}
+		<section class="block md:hidden">
+			<VotesFilterModal {votes} bind:filter bind:showFilter />
+		</section>
+	{/if}
 </div>
--- src/components/Votes/VotesFilter.svelte
+++ src/components/Votes/VotesFilter.svelte
@@ -8,7 +8,27 @@
 	export let filter: VotesFilter;
 	export let showFilter = true;
 	let resultFilter = filter.result;
+	// let catergoryFilter = [
+	// 	'Hello Mee',
+	// 	'Hello Mee',
+	// 	'Hello Mee',
+	// 	'Hello Mee',
+	// 	'Hello Mee',
+	// 	'Hello Mee',
+	// 	'Hello Mee',
+	// 	'Hello Mee',
+	// 	'Hello Mee',
+	// 	'Hello Mee',
+	// 	'Hello Mee',
+	// 	'Hello Mee',
+	// 	'Hello Mee',
+	// 	'Hello Mee',
+	// 	'Hello Mee'
+	// ];
 	let catergoryFilter = filter.category;
+
+	$: footAction =
+		filter.result.length > 0 || filter.category.length > 0 ? 'ล้างตัวเลือก' : 'เลือกทั้งหมด';
 
 	const getRowCountFilteredByCategory = (category: string) => {
 		let result = 0;
@@ -30,6 +50,22 @@
 		return result;
 	};
 
+	const doFooterAction = () => {
+		if (footAction === 'ล้างตัวเลือก') {
+			resetFilter();
+		} else {
+			selectAllFilter();
+		}
+	};
+
+	const selectAllFilter = () => {
+		filter = {
+			name: filter.name,
+			result: resultFilter,
+			category: catergoryFilter
+		};
+	};
+
 	const resetFilter = () => {
 		filter = {
 			name: '',
@@ -40,54 +76,57 @@
 </script>
 
 <div
-	class="relative md:max-w-[var(--max-width)] overflow-hidden transition-all duration-200"
+	class="relative md:max-w-[var(--max-width)] h-full overflow-hidden transition-all duration-200"
 	style="--max-width: {showFilter ? '256px' : '0px'}"
 >
 	<div class="w-[256px] relative flex flex-col h-full justify-between">
 		<button
-			class="absolute top-[0px] right-[0px] p-[10px] hover:cursor-pointer"
+			class="absolute top-[0px] right-[0px] p-[10px] hover:cursor-pointer z-10"
 			on:click={() => {
 				showFilter = !showFilter;
 			}}
 		>
 			<Minimize />
 		</button>
-		<div class="px-[24px]">
+		<div class="px-[24px] h-full flex flex-col relative">
 			<Search
 				placeholder="ค้นหาชื่อมติ/คำที่เกี่ยวข้อง"
 				bind:value={filter.name}
 				light
 				class="sticky top-0"
 			/>
-			<div class="mt-[16px]">
-				<span class="label-01 text-gray-60">ผลลัพท์</span>
-				{#each resultFilter as value}
-					<Checkbox bind:group={filter.result} {value}>
-						<div slot="labelText">
-							<span class="body-01">{value}</span>
-							<span class="body-01 text-gray-60">({getRowCountFilteredByResult(value)})</span>
-						</div>
-					</Checkbox>
-				{/each}
-			</div>
-			<div class="mt-[16px]">
-				<span class="label-01 text-gray-60">หมวดมติ</span>
-				{#each catergoryFilter as value}
-					<Checkbox bind:group={filter.category} {value}>
-						<div slot="labelText">
-							<span class="body-01">{value}</span>
-							<span class="body-01 text-gray-60">({getRowCountFilteredByCategory(value)})</span>
-						</div>
-					</Checkbox>
-				{/each}
+			<div class="overflow-scroll">
+				<div class="mt-[16px]">
+					<span class="label-01 text-gray-60">ผลลัพท์</span>
+					{#each resultFilter as value}
+						<Checkbox bind:group={filter.result} {value}>
+							<div slot="labelText">
+								<span class="body-01">{value}</span>
+								<span class="body-01 text-gray-60">({getRowCountFilteredByResult(value)})</span>
+							</div>
+						</Checkbox>
+					{/each}
+				</div>
+				<div class="mt-[16px]">
+					<span class="label-01 text-gray-60">หมวดมติ</span>
+					{#each catergoryFilter as value}
+						<Checkbox bind:group={filter.category} {value}>
+							<div slot="labelText">
+								<span class="body-01">{value}</span>
+								<span class="body-01 text-gray-60">({getRowCountFilteredByCategory(value)})</span>
+							</div>
+						</Checkbox>
+					{/each}
+				</div>
 			</div>
 		</div>
-		<button
-			class="bottom-[0px] p-[14px] w-full text-blue-90 border-blue-70 boder-solid border hover:cursor-pointer text-left"
-			on:click={resetFilter}
+		<Button
+			kind={footAction === 'ล้างตัวเลือก' ? 'tertiary' : 'secondary'}
+			class="bottom-[0px] left-[0px]  p-[14px] w-full text-left"
+			on:click={doFooterAction}
 		>
-			ล้างตัวเลือก
-		</button>
+			{footAction}
+		</Button>
 	</div>
 </div>
 
