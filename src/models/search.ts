9b4c0b7f43a25e930d5d9565dd0fbcf86f877912
Narat S <me@mixth.net>
2023-12-10 01:52:05 +0700

    feat(WEV-64): search bill proposers with searchInput

--- src/models/search.ts
+++ src/models/search.ts
@@ -2,17 +2,22 @@ import type { BillStatus } from './bill';
 import type { DefaultVotingResult } from './voting';
 
 export interface SearchIndexes {
-	politicians: {
+	politicians?: {
 		name: string;
 		description: string;
 	}[];
-	bills: {
+	bills?: {
 		name: string;
 		status: BillStatus;
 	}[];
-	votings: {
+	votings?: {
 		name: string;
 		result?: DefaultVotingResult;
+	}[];
+	billProposers?: {
+		name: string;
+		description: string;
+		proposedBillsCount: number;
 	}[];
 }
 
@@ -26,27 +31,38 @@ interface BaseSearchResultItem {
 interface PoliticianSearchResultItem extends BaseSearchResultItem {
 	billStatus?: never;
 	voteResult?: never;
+	proposedBillsCount?: never;
 }
 
 interface BillSearchResultItem extends BaseSearchResultItem {
 	billStatus: BillStatus;
 	voteResult?: never;
+	proposedBillsCount?: never;
 }
 
 interface VotingSearchResultItem extends BaseSearchResultItem {
 	billStatus?: never;
 	voteResult?: DefaultVotingResult;
+	proposedBillsCount?: never;
+}
+
+interface BillProposerSearchResultItem extends BaseSearchResultItem {
+	billStatus?: never;
+	voteResult?: never;
+	proposedBillsCount: number;
 }
 
 export type SearchResultItem =
 	| PoliticianSearchResultItem
 	| BillSearchResultItem
-	| VotingSearchResultItem;
+	| VotingSearchResultItem
+	| BillProposerSearchResultItem;
 
 export interface SearchResults {
-	politicians: PoliticianSearchResultItem[];
-	bills: BillSearchResultItem[];
-	votings: VotingSearchResultItem[];
+	politicians?: PoliticianSearchResultItem[];
+	bills?: BillSearchResultItem[];
+	votings?: VotingSearchResultItem[];
+	billProposers?: BillProposerSearchResultItem[];
 }
 
 export interface ScoreResultItem<T> {
