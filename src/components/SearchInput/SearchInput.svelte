2d960aef5657bdd5ee74cbffd47191ff8f7aede9
Narat S <me@mixth.net>
2023-12-10 00:48:23 +0700

    chore(WEV-64): pulling out SearchInput from NavigationBar to a standalone component

---
+++ src/components/SearchInput/SearchInput.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { SearchIndexes, SearchResults } from '$models/search';
+	import { TextInput } from 'carbon-components-svelte';
+	import { search } from '$lib/search';
+
+	export let searchIndexes: SearchIndexes;
+	export let searchResults: SearchResults | null;
+	export let searchValue: string | null = '';
+
+	$: if (searchValue?.trim()) {
+		searchResults = search(searchValue.trim(), searchIndexes);
+	} else {
+		searchResults = null;
+		searchValue = '';
+	}
+</script>
+
+<TextInput bind:value={searchValue} {...$$restProps} />
