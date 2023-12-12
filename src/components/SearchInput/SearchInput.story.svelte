007d3c63483dba87cf05da36a9badfedc8868b15
Th1nkK1D <witheep@gmail.com>
2023-12-12 18:28:40 +0700

    docs(wev-64): use SearchResult in SearchInput example

--- src/components/SearchInput/SearchInput.story.svelte
+++ src/components/SearchInput/SearchInput.story.svelte
@@ -3,7 +3,9 @@
 	import SearchInput from './SearchInput.svelte';
 	import type { SearchIndexes, SearchResults } from '$models/search';
 	import { searchIndexes } from '../../mocks/data/searchIndexes';
-	import { Button, CodeSnippet } from 'carbon-components-svelte';
+	import { Button } from 'carbon-components-svelte';
+	import SearchResult from '$components/SearchResult/SearchResult.svelte';
+
 	export let Hst: Hst;
 
 	let searchResults: SearchResults | null;
@@ -19,14 +21,12 @@
 	<Hst.Variant title="Default">
 		<SearchInput bind:searchResults {searchIndexes} />
 		{#if searchResults}
-			<CodeSnippet type="multi" code={JSON.stringify(searchResults, null, 2)} />
+			<SearchResult {searchResults} />
 		{/if}
 	</Hst.Variant>
 	<Hst.Variant title="Fewer Indexes">
 		<SearchInput bind:searchResults={fewerIndexesSearchResults} searchIndexes={fewerIndexes} />
-		{#if fewerIndexesSearchResults}
-			<CodeSnippet type="multi" code={JSON.stringify(fewerIndexesSearchResults, null, 2)} />
-		{/if}
+		<SearchResult searchResults={fewerIndexesSearchResults} />
 	</Hst.Variant>
 	<Hst.Variant title="With sample clear button">
 		<SearchInput
@@ -40,7 +40,7 @@
 			}}>Clear</Button
 		>
 		{#if clearSampleSearchResults}
-			<CodeSnippet type="multi" code={JSON.stringify(clearSampleSearchResults, null, 2)} />
+			<SearchResult searchResults={clearSampleSearchResults} />
 		{/if}
 	</Hst.Variant>
 </Hst.Story>
