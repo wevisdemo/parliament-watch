fbe755d93a0a9b24f4efea8692af59ad324f9fd8
Th1nkK1D <witheep@gmail.com>
2023-12-14 12:42:34 +0700

    feat(wev-31): add about page with developers list

---
+++ src/routes/about/+page.ts
@@ -0,0 +1,21 @@
+interface ContributorResponse {
+	login: string;
+	html_url: string;
+}
+
+export async function load() {
+	const res = await fetch('https://api.github.com/repos/wevisdemo/parliament-watch/contributors');
+
+	if (!res.ok) {
+		throw 'Can not fetch Github contributors';
+	}
+
+	const developers = ((await res.json()) as ContributorResponse[]).map((c) => ({
+		username: c.login,
+		url: c.html_url
+	}));
+
+	return {
+		developers
+	};
+}
