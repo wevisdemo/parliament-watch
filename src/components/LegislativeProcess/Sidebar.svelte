1128b2148decbef297eb8f9b7f9d6145fe71949f
rootEnginear <rootenginear@users.noreply.github.com>
2023-12-14 16:31:37 +0700

    fix(wev-31): sticky desktop aside

--- src/components/LegislativeProcess/Sidebar.svelte
+++ src/components/LegislativeProcess/Sidebar.svelte
@@ -16,13 +16,15 @@
 		{#each sections as section}
 			<SideNavLink
 				href="#{section.menu.replaceAll(' ', '-')}"
-				isSelected={currentNavElementId === section.menu}>{section.menu}</SideNavLink
+				isSelected={currentNavElementId === section.menu.replaceAll(' ', '-')}
+				>{section.menu}</SideNavLink
 			>
 			{#each section.menuItem as item}
 				<SideNavLink
 					class="!pl-8 !font-normal"
 					href="#{item.text.replaceAll(' ', '-')}"
-					isSelected={currentNavElementId === item.text}>{item.text}</SideNavLink
+					isSelected={currentNavElementId === item.text.replaceAll(' ', '-')}
+					>{item.text}</SideNavLink
 				>
 			{/each}
 		{/each}
