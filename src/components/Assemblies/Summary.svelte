e86adf8195e12db8f19b040891baebf1aacf5893
rootEnginear <rootEnginear@users.noreply.github.com>
2023-12-19 22:07:44 +0700

    fix(wev-86): scale seating chart

--- src/components/Assemblies/Summary.svelte
+++ src/components/Assemblies/Summary.svelte
@@ -9,7 +9,7 @@
 
 <div class="grid md:gap-[24px] gap-[16px] md:py-[32px] py-[16px]">
 	<SummaryTotal {houseLevel} data={summary.highlightGroup} />
-	<div class="grid md:grid-cols-3 grid-cols-1 gap-[8px] w-fit mx-auto w-full">
+	<div class="grid md:grid-cols-3 grid-cols-1 gap-[8px] mx-auto w-full">
 		<!-- TODO: chnage this when change phase -->
 		<!-- <div class="grid md:grid-cols-4 grid-cols-1 gap-[8px] w-fit mx-auto w-full"> -->
 		<SummaryGroup title="อายุ" data={{ memberGroups: summary.groupBySex, assemblyId }} />
--- src/components/Assemblies/SummaryTotal.svelte
+++ src/components/Assemblies/SummaryTotal.svelte
@@ -22,8 +22,24 @@
 		return result;
 	};
 
-	const lowerHouseSeatLines = [58, 54, 51, 49, 46, 43, 40, 37, 35, 32, 29, 26];
-	const upperHouseSeatLines = [39, 36, 33, 30, 28, 25, 22, 20, 17];
+	// const lowerHouseSeatLines = [58, 54, 51, 49, 46, 43, 40, 37, 35, 32, 29, 26];
+	// const upperHouseSeatLines = [39, 36, 33, 30, 28, 25, 22, 20, 17];
+
+	const LOWER_HOUSE_ROW_RATIO = [
+		0.116, 0.108, 0.102, 0.098, 0.092, 0.086, 0.08, 0.074, 0.07, 0.064, 0.058
+	];
+	const UPPER_HOUSE_ROW_RATIO = [0.156, 0.144, 0.132, 0.12, 0.112, 0.1, 0.088, 0.08];
+	const distributeLowerHouseSeatLines = (total: number, ratioTemplate: number[]) => {
+		let remaining = total;
+		let seatLine = [];
+		for (const ratio of ratioTemplate) {
+			const currentLine = Math.round(total * ratio);
+			seatLine.push(currentLine);
+			remaining -= currentLine;
+		}
+		seatLine.push(remaining);
+		return seatLine;
+	};
 
 	$: getLowerHouseTotalPartie = (): PartySeat[] => {
 		const governmentGroup = data.find((group) => group.name === 'ฝ่ายรัฐบาล');
@@ -49,7 +65,10 @@
 
 	$: seatParties = houseLevel === 'lower' ? getLowerHouseTotalPartie() : getUpperHouseTotalPartie();
 
-	$: lineAmounts = houseLevel === 'lower' ? lowerHouseSeatLines : upperHouseSeatLines;
+	$: lineAmounts = distributeLowerHouseSeatLines(
+		seatParties.reduce((a, c) => a + c.count, 0),
+		houseLevel === 'lower' ? LOWER_HOUSE_ROW_RATIO : UPPER_HOUSE_ROW_RATIO
+	);
 </script>
 
 <div class="flex md:flex-row flex-col space-y-[16px]">
