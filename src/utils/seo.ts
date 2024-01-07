708634057fa953b8b5e57df9ab3331fe4f6a4758
rootEnginear <rootEnginear@users.noreply.github.com>
2024-01-08 03:12:05 +0700

    fix: default seo, use pathname instead of route id

--- src/utils/seo.ts
+++ src/utils/seo.ts
@@ -4,10 +4,16 @@ export interface Seo {
 	og: string;
 }
 
+export const DEFAULT_SEO = {
+	title: 'Parliament Watch - ขับเคลื่อนประชาธิปไตย ร่วมเฝ้าดูความเคลื่อนไหวรัฐสภา',
+	description: 'ติดตามกฎหมาย นโยบาย และการทำงานของนักการเมือง',
+	og: 'https://parliamentwatch.wevis.info/images/og.png'
+};
+
 export const createSeo = (data: Partial<Seo>): Seo => {
 	return {
-		title: data.title ?? 'Parliament Watch - ขับเคลื่อนประชาธิปไตย ร่วมเฝ้าดูความเคลื่อนไหวรัฐสภา',
-		description: data.description ?? 'ติดตามกฎหมาย นโยบาย และการทำงานของนักการเมือง',
-		og: data.og ?? 'https://parliamentwatch.wevis.info/images/og.png'
+		title: data.title ?? DEFAULT_SEO.title,
+		description: data.description ?? DEFAULT_SEO.description,
+		og: data.og ?? DEFAULT_SEO.og
 	};
 };
