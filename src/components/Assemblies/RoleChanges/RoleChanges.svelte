5f360a6209313a7511e19b8fa0903e20ca30a0f9
sealbb <chonticha.licha@gmail.com>
2024-07-18 16:33:08 +0700

    feat: add RoleChangeItem, RoleChangeGroup, and RoleChanges components

---
+++ src/components/Assemblies/RoleChanges/RoleChanges.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { group } from 'd3';
+	import type { RoleChange } from '../../../routes/assemblies/[id]/+page.server';
+	import RoleChangeGroup from './RoleChangeGroup.svelte';
+	export let changes: Array<RoleChange>;
+	export let selectedDate: Date | undefined;
+
+	$: groupChangeData = group(changes, (d) => d?.date.toISOString());
+	$: convertAndSortChangeData = Array.from(groupChangeData, ([name, value]) => ({
+		name,
+		value
+	})).sort((a, b) => new Date(a.name).getTime() - new Date(b.name).getTime());
+</script>
+
+<div>
+	{#each convertAndSortChangeData as roleChanges}
+		{#if roleChanges}
+			<RoleChangeGroup changeData={roleChanges.value} {selectedDate} />
+		{/if}
+	{/each}
+</div>
