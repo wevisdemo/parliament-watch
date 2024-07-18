822b14906774e7ced1458d740f7dc154e6f6f776
sealbb <chonticha.licha@gmail.com>
2024-07-18 16:33:08 +0700

    feat: add RoleChangeItem, RoleChangeGroup, and RoleChanges components

---
+++ src/components/Assemblies/RoleChanges/RoleChangeGroup.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import type { RoleChange } from '../../../routes/assemblies/[id]/+page.server';
+	import Calendar from 'carbon-icons-svelte/lib/Calendar.svelte';
+	import RoleChangeItem from './RoleChangeItem.svelte';
+
+	export let changeData: RoleChange[];
+	export let selectedDate: Date | undefined;
+	const formatDate = (date: Date) =>
+		date.toLocaleDateString('th-TH', {
+			year: 'numeric',
+			month: 'short',
+			day: 'numeric'
+		});
+	$: console.log('1', selectedDate);
+	$: console.log('2', changeData[0].date);
+	$: isFocus = selectedDate?.getTime() === changeData[0].date?.getTime();
+</script>
+
+<div class={isFocus ? 'border-[1px] border-interactive-02' : ''}>
+	<div
+		class="{isFocus
+			? 'bg-interactive-02 text-text-04'
+			: 'bg-ui-01'} flex items-center justify-between px-4 py-[11px]"
+	>
+		<div class="flex items-center gap-2 {!isFocus ? 'text-text-01' : ''}">
+			<Calendar />
+			<p class="label-01">{formatDate(changeData[0].date)}</p>
+		</div>
+		<p class="heading-compact-01 {!isFocus ? 'text-text-02' : ''}">
+			ปรับ {changeData.length} ตำแหน่ง
+		</p>
+	</div>
+	<div>
+		{#each changeData as roleChangeData}
+			<RoleChangeItem
+				type={roleChangeData.type}
+				role={roleChangeData.role}
+				{...roleChangeData.politician}
+			/>
+		{/each}
+	</div>
+</div>
---
+++ src/components/Assemblies/RoleChanges/RoleChangeItem.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import PoliticianProfile from '$components/PoliticianProfile/PoliticianProfile.svelte';
+	import type { Party } from '$models/party';
+	import Login from 'carbon-icons-svelte/lib/Login.svelte';
+	import Logout from 'carbon-icons-svelte/lib/Logout.svelte';
+	export let type: 'in' | 'out';
+	export let party: Party;
+	export let avatar: string;
+	export let firstname: string;
+	export let lastname: string;
+	export let role: string;
+	export let id: string;
+
+	$: statusStyle =
+		type === 'in' ? 'bg-teal-40 translate-x-[100%] rounded-r-[4px]' : 'bg-red-60 rounded-l-[4px]';
+</script>
+
+<div class="flex h-12 items-center gap-1 px-2">
+	<div class="h-full w-12 px-2">
+		<div class="relative h-full w-1/2 border-r-[1px] border-gray-20">
+			<div
+				class="{statusStyle} absolute right-0 top-1/2 h-[24px] w-[24px] -translate-y-1/2 p-[3px]"
+			>
+				{#if type === 'in'}
+					<Login class="h-[18px] w-[18px] text-white" />
+				{:else}
+					<Logout class="h-[18px] w-[18px] rotate-180 text-white" />
+				{/if}
+			</div>
+		</div>
+	</div>
+	<div class="flex items-center">
+		<PoliticianProfile {id} {firstname} {lastname} {avatar} {party} isShowPartyName={false} />
+		<p>
+			<span class="label-02 text-text-02">เข้ารับตำแหน่ง&nbsp;</span>
+			<span class="heading-compact-01 text-text-primary">{role}</span>
+		</p>
+	</div>
+</div>
