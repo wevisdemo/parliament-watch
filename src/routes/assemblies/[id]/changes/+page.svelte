31e6719bb213b1d545780647f5a8ce87bb6d385e
Th1nkK1D <witheep@gmail.com>
2024-07-24 22:59:02 +0700

    feat: add assembly changes page with just data

---
+++ src/routes/assemblies/[id]/changes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let data;
+
+	$: ({ assembly, availableAssemblies, assemblyRoles, members } = data);
+</script>
+
+<h2 class="font-bold">assembly</h2>
+<p class="whitespace-pre">{JSON.stringify(assembly, undefined, 2)}</p>
+
+<h2 class="font-bold">availableAssemblies</h2>
+<p class="whitespace-pre">{JSON.stringify(availableAssemblies, undefined, 2)}</p>
+
+<h2 class="font-bold">assemblyRoles</h2>
+<p class="whitespace-pre">{JSON.stringify(assemblyRoles, undefined, 2)}</p>
+
+<h2 class="font-bold">members</h2>
+<p class="whitespace-pre">{JSON.stringify(members, undefined, 2)}</p>
