93bb7bf685a1596ef19635036952d6273bf5a437
JaeAiKay <78583537+JaeAiKay@users.noreply.github.com>
2024-07-29 13:30:30 +0700

    feat: add header

--- src/routes/assemblies/[id]/changes/+page.svelte
+++ src/routes/assemblies/[id]/changes/+page.svelte
@@ -1,8 +1,33 @@
 <script lang="ts">
+	import Header from '$components/Assemblies/Header.svelte';
+	import { Breadcrumb, BreadcrumbItem } from 'carbon-components-svelte';
+
 	export let data;
 
 	$: ({ assembly, availableAssemblies, assemblyRoles, members } = data);
 </script>
+
+<div class="px-[16px] md:px-[64px]">
+	<Breadcrumb
+		noTrailingSlash
+		class="my-[8px] [&>.bx--breadcrumb]:flex [&>.bx--breadcrumb]:flex-wrap"
+	>
+		<BreadcrumbItem href="/">หน้าหลัก</BreadcrumbItem>
+		<BreadcrumbItem class="hidden md:block">นักการเมือง</BreadcrumbItem>
+		<BreadcrumbItem class="hidden md:block" href="/assemblies/{assembly.id}"
+			>{assembly.name}</BreadcrumbItem
+		>
+		<BreadcrumbItem class="hidden md:block">การปรับ ครม.</BreadcrumbItem>
+	</Breadcrumb>
+	<Header
+		headerName="การปรับคณะรัฐมนตรี"
+		{assembly}
+		{availableAssemblies}
+		showStatus={false}
+		showRemark={false}
+		postfixLink="changes"
+	/>
+</div>
 
 <h2 class="font-bold">assembly</h2>
 <p class="whitespace-pre">{JSON.stringify(assembly, undefined, 2)}</p>
