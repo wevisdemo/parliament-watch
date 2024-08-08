f15bafbed7291087a3b42e54ee53740a18e3068d
JaeAiKay <78583537+JaeAiKay@users.noreply.github.com>
2024-08-09 00:56:48 +0700

    fix: hierarchy tooltip

--- src/components/Assemblies/CabinetChanges/TimeLine.svelte
+++ src/components/Assemblies/CabinetChanges/TimeLine.svelte
@@ -27,59 +27,62 @@
 	};
 </script>
 
-<div class="flex">
-	<button class="chevron w-[16px] sm:w-[64px]" on:click={handleNext}>
-		<ChevronLeft size={32} />
-	</button>
-	<Tooltip direction="top" open={true}>
-		<div slot="tooltip">
-			<TimeLineToolTip day={{ date: new Date(), in: 0, out: 0 }} selectedDate={new Date()} />
+<div class="relative">
+	<div
+		class="no-scrollbar flex overflow-x-auto px-[16px] pb-10 pt-20 sm:px-[64px]"
+		bind:this={timelineContainer}
+	>
+		<div class="chevron absolute left-0 -mt-20 w-[16px] bg-ui-01 sm:w-[64px]">
+			<button on:click={handleNext}>
+				<ChevronLeft size={32} />
+			</button>
 		</div>
-	</Tooltip>
-	<div class="no-scrollbar flex overflow-x-auto pb-10 pt-20" bind:this={timelineContainer}>
-		<div class="flex h-[65px]">
-			{#each dateData as year}
-				{#each year.months as month}
-					<div class="relative flex">
-						{#each month.days as day}
-							{@const isSelectedDate = compareDate(day.date, selectedDate)}
-							<div class="relative flex">
-								{#if day.event}
-									<div
-										class="absolute left-1 -mt-14 h-full border-l border-dashed border-black px-1 text-text-02"
-									>
-										<p class="label-01 w-fit min-w-[60px] text-text-01">
-											{formatThaiDate(day.date)}
-										</p>
-										<p class="label-01">{day.event}</p>
-									</div>
-								{/if}
-								<Tooltip
-									direction="top"
-									showAllTime={isSelectedDate}
-									open={isSelectedDate}
-									style={isSelectedDate ? 'z-index: 0;' : 'background-color: white;'}
-								>
-									<TimeItem {day} {selectedDate} {max} {handleSelectDate} />
-									<div slot="tooltip">
-										<TimeLineToolTip {day} {selectedDate} />
-									</div>
-								</Tooltip>
+		{#each dateData as year}
+			{#each year.months as month}
+				<div class="relative z-[0]">
+					<p class="label-01 absolute bottom-0 -mb-7 w-12 text-text-02">
+						{shortMonthNames[month.id]}
+						{month.id === 0 ? (year.year + 543).toString().slice(-2) : ''}
+					</p>
+				</div>
+				{#each month.days as day}
+					{@const isSelectedDate = compareDate(day.date, selectedDate)}
+					{#if day.event}
+						<div class="relative z-[0]">
+							<div
+								class="absolute left-1 -mt-14 h-full border-l border-dashed border-black px-1 text-text-02"
+							>
+								<p class="label-01 w-fit min-w-[60px] text-text-01">
+									{formatThaiDate(day.date)}
+								</p>
+								<p class="label-01">{day.event}</p>
 							</div>
-						{/each}
-
-						<div class="label-01 absolute bottom-0 -mb-7 text-text-02">
-							{shortMonthNames[month.id]}
-							{month.id === 0 ? (year.year + 543).toString().slice(-2) : ''}
 						</div>
+					{/if}
+					<div class="sticky left-0 right-0 {isSelectedDate ? 'z-[11]' : 'z-[12]'}">
+						<Tooltip
+							direction="top"
+							showAllTime={isSelectedDate}
+							open={isSelectedDate}
+							style={isSelectedDate ? '' : 'background-color: white;'}
+						>
+							<div class="h-[65px]">
+								<TimeItem {day} {selectedDate} {max} {handleSelectDate} />
+							</div>
+							<div slot="tooltip">
+								<TimeLineToolTip {day} {selectedDate} />
+							</div>
+						</Tooltip>
 					</div>
 				{/each}
 			{/each}
+		{/each}
+		<div class="chevron absolute right-0 -mt-20 w-[16px] bg-ui-01 sm:w-[64px]">
+			<button on:click={handlePrev}>
+				<ChevronRight size={32} />
+			</button>
 		</div>
 	</div>
-	<button class="chevron w-[16px] sm:w-[64px]" on:click={handlePrev}>
-		<ChevronRight size={32} />
-	</button>
 </div>
 
 <style>
@@ -95,8 +98,9 @@
 		flex: none;
 		align-items: center;
 		justify-content: center;
-		background-color: 'ui-01';
+		height: 100%;
 		padding-top: 80px;
 		padding-bottom: 40px;
+		z-index: 1;
 	}
 </style>
