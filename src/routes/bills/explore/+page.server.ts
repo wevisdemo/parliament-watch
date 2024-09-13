1a2e591298b167f526246ea4c101b593abf0c966
๋Jiratchaya Panphinij <78583537+JaeAiKay@users.noreply.github.com>
2024-09-13 16:00:26 +0700

    feat: assembly and bill explore page filter (#95)
    * feat: add logic to show proposer name for each proposer type checkbox
    
    * feat: add section in dropdown

--- src/routes/bills/explore/+page.server.ts
+++ src/routes/bills/explore/+page.server.ts
@@ -13,6 +13,7 @@ interface FilterOptions {
 	categories: string[];
 	billProposerType: BillProposerType[];
 	proposerNames: string[];
+	proposerCabinet: proposerCabinetType[];
 }
 
 interface BillSummary
@@ -25,12 +26,18 @@ interface BillSummary
 		| 'categories'
 		| 'status'
 		| 'proposerType'
+		| 'proposedByAssembly'
 		| 'proposedLedByPolitician'
 		| 'proposedByPeople'
 	> {
 	purposedAtMpAssemblyId: string;
 	proposedLedByPoliticianName?: string;
 	proposedLedByPeopleName?: string;
+}
+
+interface proposerCabinetType {
+	name: string;
+	id: string;
 }
 
 export async function load() {
@@ -74,6 +81,16 @@ export async function load() {
 		)
 	].sort((a, z) => a.localeCompare(z));
 
+	const proposerCabinet = bills
+		.filter(
+			(bill) => bill.proposerType === BillProposerType.Assembly && bill.purposedAtMpAssemblyId
+		)
+		.map((bill) => ({
+			name: `คณะรัฐมนตรี ชุดที่ ${bill.proposedByAssembly!.term} (${bill.proposedByAssembly!.startedAt.getFullYear() + 543})`,
+			id: bill.proposedByAssembly!.id
+		}))
+		.filter((cabinet, index, self) => index === self.findIndex((t) => t.id === cabinet.id));
+
 	const categories = [...new Set(bills.flatMap((bill) => bill.categories))].sort((a, z) =>
 		a.localeCompare(z)
 	);
@@ -105,7 +122,8 @@ export async function load() {
 		status: billStatuses,
 		categories,
 		billProposerType: billProposerTypes,
-		proposerNames
+		proposerNames,
+		proposerCabinet
 	};
 
 	return {
