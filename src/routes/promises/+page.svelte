69967e813eaff1ed73849c2217095049f1963a4b
Narat S <me@mixth.net>
2024-09-20 03:14:20 +0700

    feat(promise): add links to promise explore & detail page

--- src/routes/promises/+page.svelte
+++ src/routes/promises/+page.svelte
@@ -2,3 +2,8 @@
 	export let data;
 	console.log(JSON.stringify(data, null, 2));
 </script>
+
+<ul>
+	<li><a href={`/promises/1`}>Detail page</a></li>
+	<li><a href="promises/explore">Explore page</a></li>
+</ul>
