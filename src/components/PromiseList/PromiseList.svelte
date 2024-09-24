a7db4e8b7829a8ed17c59ce4c363fb803ad024cf
Chonticha Li <88100228+sealbb@users.noreply.github.com>
2024-09-24 17:05:25 +0700

    feat: create Promise List Component (#107)
    * feat: create PromiseList PromiseCard component and add to main page
    
    * fix: add key for each block
    
    * fix: fix error from key in each block
    
    * style: add color for notStarted status in promise card
    
    * fix: edit promise list component and card variables

---
+++ src/components/PromiseList/PromiseList.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+	import type { PromiseSummary } from '../../routes/promises/+page.server';
+	import { Select, SelectItem } from 'carbon-components-svelte';
+	import PromiseCard from './PromiseCard.svelte';
+	import Grid from 'carbon-icons-svelte/lib/Grid.svelte';
+	import List from 'carbon-icons-svelte/lib/List.svelte';
+
+	export let summaries: PromiseSummary[];
+	// export let cappedAt: number | undefined;
+
+	type SortByOptions = 'วันที่เคลื่อนไหวล่าสุด' | 'ตัวอักษร' | 'สถานะ';
+	let sortBy: SortByOptions = 'วันที่เคลื่อนไหวล่าสุด';
+
+	function handleChange(e: Event) {
+		const target = e.target as HTMLSelectElement;
+		const value = target?.value as SortByOptions;
+		if (
+			Object.values<SortByOptions>(['วันที่เคลื่อนไหวล่าสุด', 'ตัวอักษร', 'สถานะ']).includes(value)
+		) {
+			sortBy = value;
+		}
+	}
+
+	$: sortedData = (() => {
+		const data = Array.from(summaries);
+		switch (sortBy) {
+			case 'วันที่เคลื่อนไหวล่าสุด':
+				return data.sort(
+					(a, b) => b.latestProgressDate.date.getTime() - a.latestProgressDate.date.getTime()
+				);
+			case 'ตัวอักษร':
+				return data.sort((a, b) => (a.statements[0] ?? '').localeCompare(b.statements[0] ?? ''));
+			case 'สถานะ':
+				return data.sort((a, b) => a.status.localeCompare(b.status));
+			default:
+				return data;
+		}
+	})();
+</script>
+
+<div class="">
+	<div class="flex flex-col justify-between gap-1 pb-4 pt-6 md:flex-row md:items-center">
+		<p class="fluid-heading-03 font-bold">ผลลัพธ์ {sortedData.length} คำสัญญา</p>
+		<div class="flex items-center gap-[15px]">
+			<div class="flex items-center gap-1">
+				<p class="body-compact-01 text-text-02">เรียงตาม</p>
+				<Select
+					class="w-[228px] md:w-[288px]"
+					hideLabel
+					labelText="Carbon theme"
+					on:change={handleChange}
+				>
+					<SelectItem value="วันที่เคลื่อนไหวล่าสุด" />
+					<SelectItem value="ตัวอักษร" />
+					<SelectItem value="สถานะ" />
+				</Select>
+			</div>
+			<div class="hidden items-center gap-2 md:flex">
+				{#each [{ icon: List, label: 'List' }, { icon: Grid, label: 'Grid' }] as { icon, label } (label)}
+					<button class="h-8 w-8 rounded-[2px] p-1 duration-200 hover:bg-[#CACACA]">
+						<svelte:component this={icon} class="h-6 w-6" />
+					</button>
+				{/each}
+			</div>
+		</div>
+	</div>
+	<div class="mx-auto grid w-fit gap-6 md:grid-cols-3">
+		{#each sortedData as promiseSummary, index (index)}
+			<PromiseCard {promiseSummary} />
+		{/each}
+	</div>
+</div>
--- src/routes/promises/+page.svelte
+++ src/routes/promises/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import PromiseList from '$components/PromiseList/PromiseList.svelte';
+
 	export let data;
 	console.log(JSON.stringify(data, null, 2));
 </script>
@@ -7,3 +9,7 @@
 	<li><a href={`/promises/1`}>Detail page</a></li>
 	<li><a href="promises/explore">Explore page</a></li>
 </ul>
+
+<div class="mx-auto flex max-w-[1280px] flex-col gap-3 px-4 py-6">
+	<PromiseList summaries={data.promiseSummaries} />
+</div>
