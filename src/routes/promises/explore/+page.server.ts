b253b4159dc7a4b99b298223b5941bbd7b5af6c7
sealbb <chonticha.licha@gmail.com>
2024-09-26 01:15:53 +0700

    feat: add load url and filter data in server and add ui in explore

--- src/routes/promises/explore/+page.server.ts
+++ src/routes/promises/explore/+page.server.ts
@@ -1,7 +1,31 @@
 import { promiseSummaries } from '../../../mocks/data/promise';
 
-export async function load() {
+export async function load({ url }) {
+	const keyword = decodeURIComponent(url.searchParams.get('keyword') ?? '');
+	const category = decodeURIComponent(url.searchParams.get('category') ?? '');
+	const party = decodeURIComponent(url.searchParams.get('party') ?? '');
+
+	let filteredPromiseSummaries = promiseSummaries;
+
+	if (keyword) {
+		filteredPromiseSummaries = filteredPromiseSummaries.filter((promise) =>
+			promise.keywords.includes(keyword)
+		);
+	}
+
+	if (category) {
+		filteredPromiseSummaries = filteredPromiseSummaries.filter((promise) =>
+			promise.categories.includes(category)
+		);
+	}
+
+	if (party) {
+		filteredPromiseSummaries = filteredPromiseSummaries.filter(
+			(promise) => promise.party.name === party
+		);
+	}
+
 	return {
-		promiseSummaries
+		promiseSummaries: filteredPromiseSummaries
 	};
 }
--- src/routes/promises/explore/+page.svelte
+++ src/routes/promises/explore/+page.svelte
@@ -1,4 +1,38 @@
 <script lang="ts">
+	import { page } from '$app/stores';
+	import { Breadcrumb, BreadcrumbItem } from 'carbon-components-svelte';
+	import PromiseList from '$components/PromiseList/PromiseList.svelte';
+
 	export let data;
-	console.log(JSON.stringify(data, null, 2));
+	export let keyword: string | undefined;
+	export let category: string | undefined;
+	export let party: string | undefined;
+
+	let exploreType: string | undefined;
+
+	$: {
+		const { searchParams } = $page.url;
+		[keyword, category, party] = ['keyword', 'category', 'party'].map(
+			(param) => searchParams.get(param) || undefined
+		);
+		exploreType = keyword || category || party;
+	}
 </script>
+
+<Breadcrumb
+	noTrailingSlash
+	class="body-compact-01 px-4 py-2 [&>.bx--breadcrumb]:flex [&>.bx--breadcrumb]:flex-wrap"
+>
+	<BreadcrumbItem href="/">หน้าหลัก</BreadcrumbItem>
+	<BreadcrumbItem href="/promises">ติดตามคำสัญญา</BreadcrumbItem>
+	<BreadcrumbItem href="/assemblies/">“{exploreType}”</BreadcrumbItem>
+</Breadcrumb>
+<div class="">
+	<div class="mx-auto max-w-[1280px] px-4 py-6 md:px-16 md:py-8">
+		<h1 class="md:fluid-heading-05 fluid-heading-03">คำสัญญาทั้งหมดที่เกี่ยวข้องกับ</h1>
+		<h1 class="fluid-heading-05">{exploreType}</h1>
+	</div>
+	<div class="bg-ui-01">
+		<PromiseList summaries={data.promiseSummaries} />
+	</div>
+</div>
