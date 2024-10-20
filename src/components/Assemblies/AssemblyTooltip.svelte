c42b8fc5ffb4d8b68a39d4ff6bb873afdc6198f3
Paphawit Ngamchaliew <paphawit.ng@gmail.com>
2024-10-20 18:07:13 +0700

    fix: remove unused optional chaining

--- src/components/Assemblies/AssemblyTooltip.svelte
+++ src/components/Assemblies/AssemblyTooltip.svelte
@@ -6,12 +6,12 @@
 
 {#if tooltipProp}
 	<div
-		style:left="{tooltipProp?.x ?? 0}px"
-		style:top="{tooltipProp?.y ?? 0}px"
+		style:left="{tooltipProp.x}px"
+		style:top="{tooltipProp.y}px"
 		class="label-01 absolute z-10 flex min-w-max flex-col
 		rounded-sm bg-[#393939] p-3 text-center text-white"
 	>
-		<div class="font-semibold">{tooltipProp?.title}</div>
-		<div>{tooltipProp?.additional}</div>
+		<div class="font-semibold">{tooltipProp.title}</div>
+		<div>{tooltipProp.additional}</div>
 	</div>
 {/if}
