5d0be99214862d22cd173c2083f150d79418f53c
Paphawit Ngamchaliew <paphawit.ng@gmail.com>
2024-10-22 01:34:34 +0700

    fix: tooltip partially missing in small screen

--- src/components/Assemblies/AssemblyTooltip.svelte
+++ src/components/Assemblies/AssemblyTooltip.svelte
@@ -1,11 +1,33 @@
 <script lang="ts">
+	import { afterUpdate } from 'svelte';
 	import type { TooltipProp } from './shared';
 
 	export let tooltipProp: TooltipProp | null = null;
+	let tooltipRef: HTMLDivElement;
+	let innerWidth: number;
+
+	afterUpdate(() => {
+		if (!tooltipProp) return;
+
+		const tooltip = tooltipRef.getBoundingClientRect();
+		const { width } = tooltip;
+
+		tooltipProp.x -= width / 2;
+
+		// adjust tooltip position when its size exceed window
+		if (innerWidth < tooltipProp.x + width) {
+			tooltipProp.x = innerWidth - width;
+		} else if (tooltipProp.x < 0) {
+			tooltipProp.x = 0;
+		}
+	});
 </script>
+
+<svelte:window bind:innerWidth />
 
 {#if tooltipProp}
 	<div
+		bind:this={tooltipRef}
 		style:left="{tooltipProp.x}px"
 		style:top="{tooltipProp.y}px"
 		class="label-01 absolute z-10 flex min-w-max flex-col
