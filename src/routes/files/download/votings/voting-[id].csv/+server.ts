8da9103765f76ddbc386fdd4997449c49cb2b1a6
Th1nkK1D <witheep@gmail.com>
2025-07-02 19:10:39 +0700

    fix: use corrected matched politician data in voting votes csv

--- src/routes/files/download/votings/voting-[id].csv/+server.ts
+++ src/routes/files/download/votings/voting-[id].csv/+server.ts
@@ -1,23 +1,36 @@
 import { createCsvFileResponse } from '$lib/csv';
 import { graphql } from '$lib/politigraph';
+import { queryPoliticiansVote } from '$lib/politigraph/vote/with-politician';
 
 export const prerender = true;
 
 export async function GET({ params }) {
-	const { votes } = await graphql.query({
-		votes: {
+	const {
+		voteEvents: [voteEvent]
+	} = await graphql.query({
+		voteEvents: {
 			__args: {
 				where: {
-					vote_events_ALL: {
-						id_EQ: params.id
-					}
-				},
-				sort: [{ voter_name: 'ASC' }]
+					id_EQ: params.id
+				}
 			},
-			voter_name: true,
-			voter_party: true,
-			option: true
+			id: true,
+			organizations: {
+				id: true
+			},
+			start_date: true,
+			end_date: true
 		}
 	});
-	return createCsvFileResponse(votes);
+
+	const votes = await queryPoliticiansVote(voteEvent);
+
+	return createCsvFileResponse(
+		votes.map(({ politician, role, party, option }) => ({
+			name: politician.name,
+			role,
+			party: party?.name || '',
+			option
+		}))
+	);
 }
