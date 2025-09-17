fe2dd65544fd7cd2b7be964284b5be9fdddc6bee
Th1nkK1D <witheep@gmail.com>
2025-09-17 16:21:47 +0700

    chore: support custom politigraph graphql URL via env variable

--- src/lib/politigraph/index.ts
+++ src/lib/politigraph/index.ts
@@ -1,7 +1,7 @@
 import { createClient } from './genql';
 
 export const graphql = createClient({
-	url: 'https://politigraph.wevis.info/graphql',
+	url: import.meta.env.POLITIGRAPH_GRAPHQL_URL || 'https://politigraph.wevis.info/graphql',
 	batch: {
 		maxBatchSize: 10,
 		batchInterval: 200
