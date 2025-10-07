dd8b8601890d6a96e5ae5b9040da03ce5eae0cfc
Th1nkK1D <witheep@gmail.com>
2025-10-07 18:09:00 +0700

    feat: make politigraph graphql client support both server and client

--- src/lib/politigraph/index.ts
+++ src/lib/politigraph/index.ts
@@ -1,10 +1,14 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import { PUBLIC_POLITIGRAPH_ENDPOINT } from '$env/static/public';
+import { env } from '$env/dynamic/public';
+import * as staticEnv from '$env/static/public';
 import { createClient } from './genql';
 
 export const graphql = createClient({
-	url: PUBLIC_POLITIGRAPH_ENDPOINT || 'https://politigraph.wevis.info/graphql',
+	url:
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
+		staticEnv.PUBLIC_POLITIGRAPH_ENDPOINT ||
+		env.PUBLIC_POLITIGRAPH_ENDPOINT ||
+		'https://politigraph.wevis.info/graphql',
 	batch: {
 		maxBatchSize: 10,
 		batchInterval: 200
