bff184f9c01cd8c018be9e7156f58325c5fadf68
Th1nkK1D <witheep@gmail.com>
2025-10-07 17:24:48 +0700

    chore: add chrome devtool plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), devtoolsJson()]
 });
