228fa0f22266c9ebae15ba8135c080e84f27368b
fResult <styxmaz@gmail.com>
2025-10-14 01:04:22 +0700

    feat(crowdfunding-banner): create `CtaButton` component

---
+++ src/components/buttons/CtaButton.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Link } from 'carbon-components-svelte';
+	import { type OutboundLinkProps } from 'carbon-components-svelte/src/Link/OutboundLink.svelte';
+	import { type CarbonIconProps } from 'carbon-icons-svelte';
+	import type { SvelteComponent } from 'svelte';
+
+	export let icon: typeof SvelteComponent<CarbonIconProps>;
+	export let href: OutboundLinkProps['href'];
+	export let external = false;
+</script>
+
+<Link
+	{...$$restProps}
+	{href}
+	class={`btn-cta bx--btn bx--btn--primary p-[14px] font-bold text-text-04 visited:text-white hover:!text-white hover:no-underline ${$$restProps.class}`}
+	{icon}
+	target={external ? '_blank' : '_self'}
+>
+	<slot />
+</Link>
