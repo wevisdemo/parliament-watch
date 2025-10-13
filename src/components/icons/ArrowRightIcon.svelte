bbc4f0a046f8249c6b639b89d31a71f97a6c5557
fResult <styxmaz@gmail.com>
2025-10-14 01:05:48 +0700

    feat(crowdfunding-banner): remove `ArrowRightIcon` component

--- src/components/icons/ArrowRightIcon.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	import BaseSvg from './BaseSvg.svelte';
-</script>
-
-<BaseSvg viewBox="0 0 16 16" {...$$restProps}>
-	<path d="M9 3L8.285 3.6965L12.075 7.5H2V8.5H12.075L8.285 12.2865L9 13L14 8L9 3Z" fill="white" />
-</BaseSvg>
