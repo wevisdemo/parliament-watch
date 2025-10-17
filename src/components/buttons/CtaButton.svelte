1265cb433b1f40bc0166d0c65cfe07fcbbf7950a
fResult <styxmaz@gmail.com>
2025-10-18 01:38:24 +0700

    feat(crowdfunding-banner): update icon prop in CtaButton to support no icon

--- src/components/buttons/CtaButton.svelte
+++ src/components/buttons/CtaButton.svelte
@@ -4,15 +4,15 @@
 	import { type CarbonIconProps } from 'carbon-icons-svelte';
 	import type { SvelteComponent } from 'svelte';
 
-	export let icon: typeof SvelteComponent<CarbonIconProps>;
+	export let icon: typeof SvelteComponent<CarbonIconProps> | undefined = undefined;
 	export let href: OutboundLinkProps['href'] = '#';
 	export let external = false;
 </script>
 
 <Link
 	{...$$restProps}
-	{href}
 	class={`btn-cta bx--btn bx--btn--primary p-[14px] font-bold text-text-04 visited:text-white hover:!text-white hover:no-underline ${$$restProps.class}`}
+	{href}
 	{icon}
 	{...external && { target: '_blank', rel: 'nofollow noopener noreferrer' }}
 >
