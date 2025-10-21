0ba84af9760662ffc854185b4ae1312225370cea
Sila Setthakan-anan <styxmaz@gmail.com>
2025-10-21 16:00:14 +0700

    feat: add crowdfunding banners on home page (#206)
    * feat(5): create vote card
    
    * feat(wevisdemo#5): add story for VoteCard component
    
    * feat(wevisdemo#5): make VoteCard story component is able to custom candidate name
    
    * feat(wevisdemo#5): use Design Tokens for typography instead of TailwiindCSS utility classes
    
    * feat(wevisdemo#5): fix follow code reviewed
    
    * feat(crowdfunding-banner): create `ArrowRightIcon` component
    
    * feat(crowdfunding-banner): create `CtaButton` component
    
    * feat(crowdfunding-banner): remove `ArrowRightIcon` component
    
    * feat(crowdfunding-banner): create `CrowdfundingBanner` component
    
    * feat(crowdfunding-banner): add image for the crowdfunding full variant
    
    * feat(crowdfunding-banner): add rel=noopener when use CTA button as an external link
    
    * feat(crowdfunding-banner): add rel=noopener when use CTA button as an external link
    
    * feat(crowdfunding-banner): update icon prop in CtaButton to support no icon
    
    * feat(crowdfunding-banner): utilize `CrowdfundingBanner` component as full variant
    
    * feat(crowdfunding-banner): adjust `CrowdfundingBanner` component for full variant
    
    * fix: adjust layout
    
    * fix: responsive for full variant crowdfunding banner component
    
    * chore: update css class names
    
    * refactor: extract class names construction to builder functions
    
    * refactor: `banner-crowdfunding__root` styling
    
    * refactor: arbitrary div to `banner-crowdfunding__content-group`
    
    * refactor: enforce BEM convention
    
    * refactor: remove unused class selectors
    
    * fix(crowdfunding): reduce image size
    
    * fix(crowdfunding): remove redundant class concatenation
    
    * fix(crowdfunding): convert `variantClasses` to static constant with as const
    
    * fix(crowdfunding): remove duplicated and unused CSS
    
    * fix(crowdfunding): fix typo
    
    * feat: support mobile layout
    
    ---------
    
    Co-authored-by: Th1nkK1D <witheep@gmail.com>

---
+++ src/components/buttons/CtaButton.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Link } from 'carbon-components-svelte';
+	import { type OutboundLinkProps } from 'carbon-components-svelte/src/Link/OutboundLink.svelte';
+	import { type CarbonIconProps } from 'carbon-icons-svelte';
+	import type { SvelteComponent } from 'svelte';
+
+	export let icon: typeof SvelteComponent<CarbonIconProps> | undefined = undefined;
+	export let href: OutboundLinkProps['href'] = '#';
+	export let external = false;
+</script>
+
+<Link
+	{...$$restProps}
+	class="btn-cta bx--btn bx--btn--primary p-[14px] font-bold text-text-04 visited:text-white hover:!text-white hover:no-underline {$$restProps.class}"
+	{href}
+	{icon}
+	{...external && { target: '_blank', rel: 'nofollow noopener noreferrer' }}
+>
+	<slot />
+</Link>
