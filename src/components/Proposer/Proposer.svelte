ad9d7a4fb7046d2c2d160db30f588312e1780d81
Th1nkK1D <witheep@gmail.com>
2025-11-08 00:17:29 +0700

    refactor: migrate individual bill page, except event section, to use politigraph data

--- src/components/Proposer/Proposer.svelte
+++ src/components/Proposer/Proposer.svelte
@@ -2,31 +2,25 @@
 	export type PoliticianProposer = {
 		id: string;
 		name: string;
-		image: string | null;
-		assembly?: {
-			id: string;
-			abbreviation: string | null;
-			term: number | null;
-			startedAt: Date | string | null;
-		};
+		image?: string | null;
+		assemblyPost?: string;
+		assembly?: AssemblyProposer;
 		partyName?: string;
 	};
 
 	export type AssemblyProposer = {
 		id: string;
-		abbreviation: string;
-		term: number;
-		startedAt: Date | string | null;
+		name: string;
+		founding_date: Date | string | null;
 	};
 
 	export type PeopleProposer = {
-		ledBy: string;
+		name: string;
 		signatoryCount: number;
 	};
 </script>
 
 <script lang="ts">
-	import GeneralIcon from '$components/icons/GeneralIcon.svelte';
 	import PeopleIcon from '$components/icons/PeopleIcon.svelte';
 	import PoliticianIcon from '$components/icons/PoliticianIcon.svelte';
 	import dayjs from 'dayjs';
@@ -50,8 +44,33 @@
 <div class="flex items-center {isLandscape ? 'flex-col gap-x-2 md:flex-row' : 'flex-col'}">
 	{#if proposer === undefined}
 		<p class="text-sm text-gray-60">ไม่พบข้อมูล</p>
-	{:else if 'image' in proposer}
-		{@const { id, name, image, assembly, partyName } = proposer}
+	{:else if 'founding_date' in proposer}
+		<!-- Assembly -->
+		{@const { id, name, founding_date } = proposer}
+		<div class="flex h-6 w-6 items-center justify-center rounded-full bg-black">
+			<PoliticianIcon class="stroke-white" size={16} />
+		</div>
+		<a href="/assemblies/{id}" class="text-sm text-black underline">
+			{name}
+			{#if founding_date}
+				({getBudistYear(founding_date)})
+			{/if}
+		</a>
+	{:else if 'signatoryCount' in proposer}
+		<!-- People -->
+		{@const { name, signatoryCount } = proposer}
+		<div class="flex h-6 w-6 items-center justify-center rounded-full bg-black">
+			<PeopleIcon class="stroke-white" size={16} />
+		</div>
+		<p class="text-sm text-black">
+			{name}
+			{#if signatoryCount}
+				<span class="text-gray-60">และประชาชน {signatoryCount.toLocaleString()} คน</span>
+			{/if}
+		</p>
+	{:else}
+		<!-- Politicians -->
+		{@const { id, name, image, assemblyPost, assembly, partyName } = proposer}
 		<figure class="h-6 w-6 shrink-0 overflow-hidden rounded-full bg-gray-20">
 			<img
 				src={image || '/images/politicians/_placeholder.webp'}
@@ -66,44 +85,15 @@
 			</a>
 			{#if assembly}
 				<a href="/assemblies/{assembly.id}" class="text-sm text-black underline">
-					{assembly.abbreviation} ชุดที่ {assembly.term}
-					{#if assembly.startedAt}
-						({getBudistYear(assembly.startedAt)})
+					{assemblyPost || assembly.name}
+					{#if assembly.founding_date}
+						({getBudistYear(assembly.founding_date)})
 					{/if}
 				</a>
 			{/if}
 		</p>
 		{#if partyName}
-			<span class="text-sm text-gray-60">พรรค{partyName}</span>
+			<p class="text-sm text-gray-60">พรรค{partyName}</p>
 		{/if}
-	{:else if 'term' in proposer}
-		{@const { id, abbreviation, term, startedAt } = proposer}
-		<div class="flex h-6 w-6 items-center justify-center rounded-full bg-black">
-			<svelte:component
-				this={abbreviation === 'ครม.' ? GeneralIcon : PoliticianIcon}
-				class="stroke-white"
-				size={16}
-			/>
-		</div>
-		<a href="/assemblies/{id}" class="text-sm text-black">
-			{abbreviation}
-			<span class="underline"
-				>ชุดที่ {term}
-				{#if startedAt}
-					({getBudistYear(startedAt)})
-				{/if}
-			</span>
-		</a>
-	{:else}
-		{@const { ledBy, signatoryCount } = proposer}
-		<div class="flex h-6 w-6 items-center justify-center rounded-full bg-black">
-			<PeopleIcon class="stroke-white" size={16} />
-		</div>
-		<p class="text-sm text-black">
-			{ledBy}
-			{#if signatoryCount}
-				<span class="text-gray-60">และประชาชน {signatoryCount.toLocaleString()} คน</span>
-			{/if}
-		</p>
 	{/if}
 </div>
