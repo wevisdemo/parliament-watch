7141bd20272d905f7dba94bd74f0f8f952e9a1b3
Th1nkK1D <witheep@gmail.com>
2025-11-12 12:08:27 +0700

    refactor: split links cell component for data table

---
+++ src/components/DataPage/LinksCell.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { DocumentPdf, NotebookReference } from 'carbon-icons-svelte';
+
+	export let cellValue: { note: string; url: string }[] = [];
+</script>
+
+{#if cellValue.length}
+	<div class="flex flex-row gap-2">
+		{#each cellValue as { note, url } (note)}
+			<a href={url} target="_blank" rel="noopener noreferrer" title={note}
+				><svelte:component this={url.includes('.pdf') ? DocumentPdf : NotebookReference}
+				></svelte:component><span class="sr-only">{note}</span></a
+			>
+		{/each}
+	</div>
+{:else}
+	<span>-</span>
+{/if}
