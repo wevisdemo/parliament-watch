f7d1969a4ff6cc5f64f8a3dcacbb00da4c02ecf1
Th1nkK1D <witheep@gmail.com>
2025-11-28 22:17:11 +0700

    feat: use politigraph data for bill search index

--- src/routes/files/search-indexes/[category].json/+server.ts
+++ src/routes/files/search-indexes/[category].json/+server.ts
@@ -1,10 +1,6 @@
-import { fetchBills, fetchPromises } from '$lib/datasheets';
 import { graphql } from '$lib/politigraph';
-import { BillProposerType } from '$models/bill';
-import type { AssemblyRoleHistory, PartyRoleHistory } from '$models/politician';
 import { SearchIndexCategory, type SearchIndexes } from '$models/search';
 import { error } from '@sveltejs/kit';
-import { rollup } from 'd3-array';
 
 export const prerender = true;
 
@@ -79,60 +75,28 @@ export async function GET({ params }) {
 		}
 
 		case SearchIndexCategory.Bills: {
-			// const indexes: SearchIndexes['bills'] = (await fetchBills())
-			// 	.map(({ id, nickname, status }) => ({ id, name: nickname, status }))
-			// 	.sort((a, z) => a.name.localeCompare(z.name));
+			const { bills } = await graphql.query({
+				bills: {
+					__args: {
+						sort: [{ title: 'ASC' }]
+					},
+					id: true,
+					title: true,
+					nickname: true,
+					status: true
+				}
+			});
 
-			return createJSONFileResponse([]);
+			const indexes: SearchIndexes['bills'] = bills.map(({ id, title, nickname, status }) => ({
+				id,
+				name: nickname || title,
+				status
+			}));
+
+			return createJSONFileResponse(indexes);
 		}
 
 		case SearchIndexCategory.BillProposers: {
-			// const proposers = (await fetchBills())
-			// 	.map((bill) => {
-			// 		if (bill.proposedLedByPolitician) {
-			// 			const { firstname, lastname, assemblyRoles, partyRoles } = bill.proposedLedByPolitician;
-			// 			const billProposedOn = bill.proposedOn.getTime();
-
-			// 			const matchedAssemblyRole = assemblyRoles?.find(
-			// 				({ startedAt, endedAt }) =>
-			// 					billProposedOn >= startedAt.getTime() &&
-			// 					(!endedAt || billProposedOn <= endedAt.getTime())
-			// 			);
-
-			// 			const matchedPartyRole = partyRoles?.find(
-			// 				({ startedAt, endedAt }) =>
-			// 					billProposedOn >= startedAt.getTime() &&
-			// 					(!endedAt || billProposedOn <= endedAt.getTime())
-			// 			);
-
-			// 			return {
-			// 				name: `${firstname} ${lastname}`,
-			// 				description:
-			// 					getPoliticianDescription(matchedAssemblyRole, matchedPartyRole) ||
-			// 					BillProposerType.Politician
-			// 			};
-			// 		} else if (bill.proposedByPeople) {
-			// 			return {
-			// 				name: bill.proposedByPeople.ledBy,
-			// 				description: BillProposerType.People
-			// 			};
-			// 		}
-
-			// 		return { name: '', description: '' };
-			// 	})
-			// 	.filter(({ name }) => name);
-
-			// const indexes: SearchIndexes['billProposers'] = [
-			// 	...rollup(
-			// 		proposers,
-			// 		(group) => ({
-			// 			...group[0],
-			// 			proposedBillsCount: group.length
-			// 		}),
-			// 		({ name }) => name
-			// 	).values()
-			// ].sort((a, z) => a.name.localeCompare(z.name));
-
 			return createJSONFileResponse([]);
 		}
 
@@ -161,14 +125,6 @@ export async function GET({ params }) {
 		}
 
 		case SearchIndexCategory.Promises: {
-			// const indexes: SearchIndexes['promises'] = (await fetchPromises()).map(
-			// 	({ id, statements, status }) => ({
-			// 		id,
-			// 		name: statements[0],
-			// 		status
-			// 	})
-			// );
-
 			return createJSONFileResponse([]);
 		}
 
@@ -176,17 +132,6 @@ export async function GET({ params }) {
 			error(404);
 	}
 }
-
-const getPoliticianDescription = (
-	assemblyRole?: Omit<AssemblyRoleHistory, 'politicianId'>,
-	partyRole?: Omit<PartyRoleHistory, 'politicianId'>
-) =>
-	[
-		assemblyRole ? assemblyRole.assembly.abbreviation + assemblyRole.appointmentMethod : null,
-		partyRole?.party.name
-	]
-		.filter((text) => text)
-		.join(' | ');
 
 const createJSONFileResponse = (indexes: SearchIndexes[SearchIndexCategory]) =>
 	new Response(JSON.stringify(indexes), {
