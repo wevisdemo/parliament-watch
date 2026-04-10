8ab3ca63041b1683271bf67c167ec61048d20d0b
Th1nkK1D <witheep@gmail.com>
2026-04-10 14:17:01 +0700

    feat: update assembly voting header to match bill explore page (#239)

--- src/components/Assemblies/Votes/VotesHeader.svelte
+++ src/components/Assemblies/Votes/VotesHeader.svelte
@@ -5,6 +5,7 @@
 
 	export let name: string;
 	export let id: string;
+	export let voteCount: number;
 	export let availableAssemblies: AvailableAssembly[];
 
 	const getAssemblyPath = (assembly: AvailableAssembly) =>
@@ -15,9 +16,12 @@
 	class="flex w-full flex-col justify-between bg-field-01 px-[16px] py-[16px] md:flex-row md:px-[64px] md:py-[48px]"
 >
 	<div class="flex w-full max-w-[900px] items-center">
-		<div class="flex flex-col md:flex-row">
-			<h2 class="fluid-heading-04">{name}</h2>
-			<AssemblyIdRunner {id} {availableAssemblies} {getAssemblyPath} />
+		<div class="flex flex-col gap-1">
+			<div class="flex flex-col md:flex-row">
+				<h2 class="fluid-heading-04">ประวัติการลงมติ{name.split(' ').at(0)}</h2>
+				<AssemblyIdRunner {id} {availableAssemblies} {getAssemblyPath} />
+			</div>
+			<p class="body-01">ผลลัพธ์จากเงื่อนไขการกรอง: {voteCount} มติ</p>
 		</div>
 	</div>
 </div>
