e57dcc0b5a1d8f35c9253a8c79c5338ea3851476
Th1nkK1D <witheep@gmail.com>
2026-06-26 11:38:35 +0100

    fix: resolve carbon-components-svelte imports in vite 8 during bundle

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import devtoolsJson from 'vite-plugin-devtools-json';
 
 export default defineConfig({
 	plugins: [sveltekit(), devtoolsJson()],
+	optimizeDeps: {
+		exclude: ['carbon-components-svelte', 'carbon-icons-svelte']
+	},
+	ssr: {
+		noExternal: ['carbon-components-svelte', 'carbon-icons-svelte']
+	},
 	css: {
 		preprocessorOptions: {
 			scss: {
